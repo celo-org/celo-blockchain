dcb439922a37d87d924bc562208082fd5bade336
Asa Oines <asaj@Asas-MacBook-Pro.local>
2018-08-15 12:12:20 -0700

    s/PROJECT_NAME/PROJECT_ID/g

--- dockerize_testnet.sh
+++ dockerize_testnet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PROJECT_NAME=false
+PROJECT_ID=false
 TESTNET_NAME=false
 
 while getopts 'p:t:' flag; do
   case "${flag}" in
-    p) PROJECT_NAME="${OPTARG}" ;;
+    p) PROJECT_ID="${OPTARG}" ;;
     t) TESTNET_NAME="${OPTARG}" ;;
     *) error "Unexpected option ${flag}" ;;
   esac
@@ -12,9 +12,9 @@ done
 
 docker build . -t testnet-geth
 docker build . -f Dockerfile.kubeboot -t testnet-boot
-docker tag testnet-geth:latest gcr.io/$PROJECT_NAME/testnet-geth:$TESTNET_NAME
-docker tag testnet-boot:latest gcr.io/$PROJECT_NAME/testnet-boot:$TESTNET_NAME
-docker push gcr.io/$PROJECT_NAME/testnet-geth:$TESTNET_NAME
-docker push gcr.io/$PROJECT_NAME/testnet-boot:$TESTNET_NAME
+docker tag testnet-geth:latest gcr.io/$PROJECT_ID/testnet-geth:$TESTNET_NAME
+docker tag testnet-boot:latest gcr.io/$PROJECT_ID/testnet-boot:$TESTNET_NAME
+docker push gcr.io/$PROJECT_ID/testnet-geth:$TESTNET_NAME
+docker push gcr.io/$PROJECT_ID/testnet-boot:$TESTNET_NAME
 
 
