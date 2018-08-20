2b004e6d93a794f9b12a544ff9634e94fe563229
Asa Oines <asaj@Asas-MacBook-Pro.local>
2018-08-19 18:37:34 -0700

    Fix input validation

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -381,7 +381,8 @@ func (c *requestVerificationMessage) RequiredGas(input []byte) uint64 {
 // data[128:128 + encryptedPhoneLength] bytes encryptedPhone
 func (c *requestVerificationMessage) Run(input []byte) ([]byte, error) {
 	encryptedPhoneLength := int(input[127])
-	expectedInputLength := (32 * 4) + encryptedPhoneLength/32
+	// Input to call() expected to be a multiple of 32 bytes
+	expectedInputLength := 32 * (5 + encryptedPhoneLength/32)
 	// The minimum length of a valid international phone number is 7 digits
 	if encryptedPhoneLength <= 7 || len(input) != expectedInputLength {
 		err := errors.New("Provided input to requestVerificationMessage is not of valid length")
