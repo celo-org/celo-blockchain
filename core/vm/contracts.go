32a2bf7f761be59f5e762a91993d65cec5eef8c4
Asa Oines <asaj@Asas-MacBook-Pro.local>
2018-08-29 12:50:54 -0700

    Wasn't compiling

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -48,7 +48,7 @@ var PrecompiledContractsHomestead = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{4}): &dataCopy{},
 }
 
-var CeloPrecompiledContractsAddressOffset = []byte{0xff}
+var CeloPrecompiledContractsAddressOffset = byte(0xff)
 var requestVerificationAddress = common.BytesToAddress(append([]byte{0}, CeloPrecompiledContractsAddressOffset))
 
 // PrecompiledContractsByzantium contains the default set of pre-compiled Ethereum
