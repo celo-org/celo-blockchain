c5376210f69a2754aeea6b4da7873f72e637efdf
Ashish Bhatia <ashishb@ashishb.net>
2018-10-25 15:53:09 -0700

    Make Geth for mobile's sync mode configurable
    1. Default to LightSync to maintain backward compatibility.
    2. Gomobile, the tool for generating Android bindings does not handle
    enums, so, a new set of integer constants have to be created.

--- mobile/geth.go
+++ mobile/geth.go
@@ -38,6 +38,14 @@ import (
 	whisper "github.com/ethereum/go-ethereum/whisper/whisperv6"
 )
 
+// I am intentionally duplicating these constants different from downloader.SyncMode integer values, to ensure the
+// backwards compatibility where the mobile node defaults to LightSync.
+const SyncModeUnset = 0 // will be treated as SyncModeLightSync
+const SyncModeFullSync = 1
+const SyncModeFastSync = 2
+const SyncModeLightSync = 3
+const SyncModeCeloLatestSync = 4
+
 // NodeConfig represents the collection of configuration values to fine tune the Geth
 // node embedded into a mobile process. The available values are a subset of the
 // entire API provided by go-ethereum to reduce the maintenance surface and dev
@@ -76,6 +84,14 @@ type NodeConfig struct {
 
 	// Listening address of pprof server.
 	PprofAddress string
+
+	// Sync mode for the node (eth/downloader/modes.go)
+	// This has to be integer since Enum exports to Java are not supported by "gomobile"
+	// FullSync = 1
+	// FastSync = 2
+	// LightSync = 3 (default)
+	// CeloLatestSync = 4
+	SyncMode int
 }
 
 // defaultNodeConfig contains the default node configuration values to use if all
@@ -162,7 +178,7 @@ func NewNode(datadir string, config *NodeConfig) (stack *Node, _ error) {
 		// Once https://github.com/celo-org/geth/pull/62 is landed,
 		// change the ethConf.SyncMode to downloader.CeloLatestSync.
 		// It might be better to make this configurable.
-		ethConf.SyncMode = downloader.LightSync
+		ethConf.SyncMode = getSyncMode(config.SyncMode)
 		ethConf.NetworkId = uint64(config.EthereumNetworkID)
 		ethConf.DatabaseCache = config.EthereumDatabaseCache
 		if err := rawStack.Register(func(ctx *node.ServiceContext) (node.Service, error) {
@@ -191,6 +207,24 @@ func NewNode(datadir string, config *NodeConfig) (stack *Node, _ error) {
 		}
 	}
 	return &Node{rawStack}, nil
+}
+
+func getSyncMode(syncMode int) downloader.SyncMode {
+	switch syncMode {
+	case SyncModeFullSync:
+		return downloader.FullSync
+	case SyncModeFastSync:
+		return downloader.FastSync
+	case SyncModeUnset: fallthrough
+		// If unset, default to light sync.
+		// This maintains backward compatibility.
+	case SyncModeLightSync:
+		return downloader.LightSync
+	case SyncModeCeloLatestSync:
+		return downloader.CeloLatestSync
+	default:
+		panic(fmt.Sprintf("Unexpected sync mode value: %d", syncMode))
+	}
 }
 
 // Start creates a live P2P node and starts running it.
