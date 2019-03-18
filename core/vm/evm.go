1b235a2d011c62cb72c15ed6cd4516c5731214ce
Yasmeen Roumie <y@celo.org>
2019-03-18 13:40:08 -0700

    Import log and hexutil

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -25,8 +25,10 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 )
 
