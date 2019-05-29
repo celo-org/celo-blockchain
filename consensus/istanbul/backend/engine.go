0f22afde633d0ef6bb8d45cc2824a8fc95cf0122
jarmg <jaredgreene1@gmail.com>
2019-05-28 17:39:17 -0700

    Respond to PR comments

--- consensus/istanbul/backend/engine.go
+++ consensus/istanbul/backend/engine.go
@@ -428,8 +428,11 @@ func (sb *Backend) IsLastBlockOfEpoch(header *types.Header) bool {
 // consensus rules that happen at finalization (e.g. block rewards).
 func (sb *Backend) Finalize(chain consensus.ChainReader, header *types.Header, state *state.StateDB, txs []*types.Transaction,
 	uncles []*types.Header, receipts []*types.Receipt) (*types.Block, error) {
-	// No block rewards in Istanbul, so the state remains as is and uncles are dropped
-	sb.updateGasPriceSuggestion(state)
+
+  // Calculate a new gas price suggestion and push it to the GasPriceOracle SmartContract
+  sb.updateGasPriceSuggestion(state)
+
+  // No block rewards in Istanbul, so the state remains as is and uncles are dropped
 	header.Root = state.IntermediateRoot(chain.Config().IsEIP158(header.Number))
 	header.UncleHash = nilUncleHash
 
