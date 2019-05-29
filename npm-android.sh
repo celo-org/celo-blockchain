c6fa9001678f4e8ef5a7ef10f705c56daeb4c9d8
Connor McEwen <connor.mcewen@gmail.com>
2019-05-28 19:04:44 -0700

    Try again

--- npm-android.sh
+++ npm-android.sh
@@ -7,7 +7,7 @@ VERSION=$(npm show @celo/client version)
 a=( ${VERSION//./ } )
 ((a[2]++))
 
-export NPM_TOKEN=$2
+echo //registry.npmjs.org/:_authToken=$2 > ~/.npmrc
 npm -f --no-git-tag-version version "${a[0]}.${a[1]}.${a[2]}"
 PACKAGE=$(npm pack)
 npm publish $PACKAGE --tag $1
