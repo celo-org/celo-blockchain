5fe8b82f6b1d45a04d5061e93fb0962d5aabea16
Connor McEwen <connor.mcewen@gmail.com>
2019-05-28 21:45:43 -0700

    one more

--- npm-android.sh
+++ npm-android.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 # make android
 
-# rm README.md
-# rm README.celo.md
-# VERSION=$(npm show @celo/client version)
-# a=( ${VERSION//./ } )
-# ((a[2]++))
+rm README.md
+rm README.celo.md
+VERSION=$(npm show @celo/client version)
+a=( ${VERSION//./ } )
+((a[2]++))
 
 echo //registry.npmjs.org/:_authToken=$2 > ~/.npmrc
-cat ~/.npmrc
-# npm -f --no-git-tag-version version "${a[0]}.${a[1]}.${a[2]}"
-# PACKAGE=$(npm pack)
-# npm publish $PACKAGE --tag $1 --access public
+npm -f --no-git-tag-version version "${a[0]}.${a[1]}.${a[2]}"
+PACKAGE=$(npm pack)
+npm publish $PACKAGE --tag $1 --access public
