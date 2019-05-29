18932691c2f05a5618bcc7661bbb1509d0a9d863
Connor McEwen <connor.mcewen@gmail.com>
2019-05-28 21:39:54 -0700

    testing

--- npm-android.sh
+++ npm-android.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # make android
 
-rm README.md
-rm README.celo.md
-VERSION=$(npm show @celo/client version)
-a=( ${VERSION//./ } )
-((a[2]++))
+# rm README.md
+# rm README.celo.md
+# VERSION=$(npm show @celo/client version)
+# a=( ${VERSION//./ } )
+# ((a[2]++))
 
 echo //registry.npmjs.org/:_authToken=$2 > ~/.npmrc
-npm -f --no-git-tag-version version "${a[0]}.${a[1]}.${a[2]}"
-PACKAGE=$(npm pack)
-npm whoami
-npm publish $PACKAGE --tag $1 --access public
+cat ~/.npmrc
+# npm -f --no-git-tag-version version "${a[0]}.${a[1]}.${a[2]}"
+# PACKAGE=$(npm pack)
+# npm publish $PACKAGE --tag $1 --access public
