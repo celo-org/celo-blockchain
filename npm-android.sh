da5b043153417efdf29fd9f26dd93b6c91323676
Connor McEwen <connor.mcewen@gmail.com>
2019-05-28 17:21:05 -0700

    Build and push to npm

---
+++ npm-android.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+make android
+
+rm README.md
+rm README.celo.md
+VERSION=$(npm show @celo/client version)
+a=( ${VERSION//./ } )
+((a[2]++))
+
+npm -f --no-git-tag-version version "${a[0]}.${a[1]}.${a[2]}"
+PACKAGE=$(npm pack)
+npm publish $PACKAGE --tag $COMMIT_SHA
