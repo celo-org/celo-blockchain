2ce9a78f7ffac306898b087e3a31ccb19886a175
Connor McEwen <connor.mcewen@gmail.com>
2019-05-28 21:53:09 -0700

    use latest tag

--- npm-android.sh
+++ npm-android.sh
@@ -8,6 +8,8 @@ a=( ${VERSION//./ } )
 ((a[2]++))
 
 echo //registry.npmjs.org/:_authToken=$2 > ~/.npmrc
-npm -f --no-git-tag-version version "${a[0]}.${a[1]}.${a[2]}"
+NEW_VERSION="${a[0]}.${a[1]}.${a[2]}"
+npm -f --no-git-tag-version version $NEW_VERSION
 PACKAGE=$(npm pack)
 npm publish $PACKAGE --tag $1 --access public
+npm dist-tag add @celo/client@$NEW_VERSION latest
