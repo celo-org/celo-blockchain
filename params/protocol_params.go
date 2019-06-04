348e0761c4d1d2883114c2ed510b3dac27a6cfee
jarmg <jaredgreene1@gmail.com>
2019-06-04 12:09:01 -0700

    Enable smart contract calling from LES (#207)
    * initial checkin for reading validator set from a smart contract.  Still a WIP.
    
    * Implement currency conversion in the PriceComparator
    
    * changes for retrieving validator set from sc and saving validator set diff in the headers
    
    * fixed the golang test to work
    
    * fixed a few bugs
    
    * fixed a bug
    
    * fixed some tests
    
    * fixed lint errors
    
    * fixed error in snapshot application and modified snapshot test to test validator set diffs in the header
    
    * added another testcase for the snapshot
    
    * added test for validator set diff function
    
    * fixed lint issues
    
    * Calculate gasprice base on multiple-currency txs
    
    Calculate the gasprice for the gasprice Oracle in gold based on
    txs which paid gasprice in multiple currencies.
    
    * changed istanbul acceptors to validate block and changes based on PR.
    
    * disabled a few test cases (for now)
    
    * Implement currency conversion in the PriceComparator
    
    * Calculate gasprice base on multiple-currency txs
    
    Calculate the gasprice for the gasprice Oracle in gold based on
    txs which paid gasprice in multiple currencies.
    
    * Remove gpo from light client
    
    * Add internal EVM handler to Istanbul Backend
    
    * Generalize EVM signature and add to the Ethereum object
    
    * Add InternalEVMHandler to Light Ethereum Struct
    
    * Call GasPriceOracle SC from Geth
    
    * Add gaspricesuggestion update function to Finalize
    
    * Add SC call to LES api_backend
    
    * Add an address registry check for newly connected light clients
    
    * Clean up logging
    
    * Remove unrelated changes from rebase
    
    * Clean up some code placement and TODOs for PR
    
    * Clean up some code placement and TODOs for PR
    
    * Reorder function for readability
    
    * Reorder function for readability
    
    * Simplify iEvmH constructions
    
    * Edits based on gofmt
    
    * Edit test Istanbul signatures
    
    * Edit test Istanbul signatures
    
    * Fix linting error
    
    * Refactor comments and implement other small PR change requests
    
    * Simplify EVM abstraction with better use of blockchain objects
    
    * Implement formatting fixes from gofmt
    
    * Move business logic out of api_backend
    
    * Edits based on gofmt
    
    * Add copyright info to new file
    
    * Fix lightchain signatures in tests
    
    * Fix nil currency error
    
    * Add logging for conversion failure
    
    * Respond to PR comments
    
    * Respond to PR comments
    
    * Implement changes in response to PR
    
    * Remove iEvmH from Consensus Engine Construction
    
    Remove iEvmH from Consensus Engine Construction in order to simplify
    signature and remove duplicate iEvmH setting.
    
    * Edits based on gofmt
    
    * Fix test signatures
    
    * Remove whitespace
    
    * Update ChainContext comment
    
    * Edits based on gofmt
    
    * Refactor Price Comparator and remove vmConfig from lightchain
    
    * Add address refresh on les construction and remove func
    
    * Respond to PR comments
    
    * Handle concurrency in CurrencyOperator
    
    * Add temporary test logging
    
    * Remove test logging statements
    
    * Add comment for registered address refresh

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -101,6 +101,7 @@ const (
 	SortedOraclesRegistryId          = "SortedOracles"
 	GasCurrencyWhitelistRegistryId   = "GasCurrencyWhitelist"
 	ValidatorsRegistryId             = "Validators"
+	GasPriceOracleRegistryId         = "GasPriceOracle"
 )
 
 var (
