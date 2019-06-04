7c2248ad74558c37e66601532823c3b6b9ef28b9
Kevin Jue <kevin@celo.org>
2019-06-04 10:43:39 -0700

    removed some debugging log messages

--- p2p/server.go
+++ p2p/server.go
@@ -488,10 +488,8 @@ func (srv *Server) setupLocalNode() error {
 	}
 	switch srv.NAT.(type) {
 	case nil:
-		srv.log.Info("setupLocalNode", "NAT is nil")
 		// No NAT interface, do nothing.
 	case nat.ExtIP:
-		srv.log.Info("setupLocalNode", "NAT is ExtIP")
 		// ExtIP doesn't block, set the IP right away.
 		ip, _ := srv.NAT.ExternalIP()
 		srv.localnode.SetStaticIP(ip)
@@ -500,10 +498,8 @@ func (srv *Server) setupLocalNode() error {
 		// do it in the background.
 		srv.loopWG.Add(1)
 		go func() {
-			srv.log.Info("setupLocalNode", "NAT is going to ask the router")
 			defer srv.loopWG.Done()
 			if ip, err := srv.NAT.ExternalIP(); err == nil {
-				srv.log.Info("setupLocalNode", "NAT is ExternalIP")
 				srv.localnode.SetStaticIP(ip)
 			}
 		}()
@@ -581,25 +577,8 @@ func (srv *Server) setupListening() error {
 	}
 	laddr := listener.Addr().(*net.TCPAddr)
 	srv.ListenAddr = laddr.String()
-	srv.log.Info("setupListening", "ListenAddr", srv.ListenAddr)
 	srv.listener = listener
 	srv.localnode.Set(enr.TCP(laddr.Port))
-
-	/*addrs, err := net.InterfaceAddrs()
-
-	for _, addr := range addrs {
-		var ip net.IP
-		switch v := addr.(type) {
-		case *net.IPNet:
-			ip = v.IP
-		case *net.IPAddr:
-			ip = v.IP
-		default:
-			continue
-		}
-		// process IP address
-		srv.log.Info("setupListening", "addr", ip.To4())
-	}*/
 
 	srv.loopWG.Add(1)
 	go srv.listenLoop()
