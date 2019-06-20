844ed0805152dc8a4922bf64456fde33f0aa0b25
Brice Wang <brice.l.wang@gmail.com>
2019-06-20 14:00:19 -0700

    Gofmt fix

--- consensus/istanbul/backend/engine.go
+++ consensus/istanbul/backend/engine.go
@@ -28,7 +28,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/consensus/istanbul"
-	"github.com/ethereum/go-ethereum/core"
 	istanbulCore "github.com/ethereum/go-ethereum/consensus/istanbul/core"
 	"github.com/ethereum/go-ethereum/consensus/istanbul/validator"
 	"github.com/ethereum/go-ethereum/core"
