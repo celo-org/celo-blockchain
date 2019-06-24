30bbfff1e1fc55fa74bce51960ec9e3e1a471df7
Asa Oines <a@celo.org>
2019-06-24 14:01:32 -0700

    Cleanup

--- core/state_processor.go
+++ core/state_processor.go
@@ -24,7 +24,6 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 )
 
