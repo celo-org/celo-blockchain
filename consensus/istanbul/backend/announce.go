7705c81412e607ac21636a7a7959011ed78e9c97
Nicholas Guo <nguo@celo.org>
2019-07-07 18:11:57 -0700

    lint

--- consensus/istanbul/backend/announce.go
+++ consensus/istanbul/backend/announce.go
@@ -368,7 +368,7 @@ func (sb *Backend) handleIstAnnounce(payload []byte) error {
 		block := sb.currentBlock()
 		valSet := sb.getValidators(block.Number().Uint64(), block.Hash())
 
-		if enodeUrl != msg.EnodeUrl {
+		if enodeUrl != msg.EnodeURL {
 			sb.logger.Warn("Should fail here") //TODO(nguo) remove this
 		}
 
