73c1d86b519c72722a924664304e6573cbcf77fa
Asa Oines <a@celo.org>
2019-07-15 23:16:41 -0700

    Provide 10M gas for fetching the validator set (#346)

--- consensus/istanbul/backend/engine.go
+++ consensus/istanbul/backend/engine.go
@@ -428,7 +428,7 @@ func (sb *Backend) getValSet(header *types.Header, state *state.StateDB) ([]comm
 		return newValSet, err
 	} else {
 		// Get the new epoch's validator set
-		maxGasForGetValidators := uint64(1000000)
+		maxGasForGetValidators := uint64(10000000)
 		// TODO(asa) - Once the validator election smart contract is completed, then a more accurate gas value should be used.
 		_, err := sb.iEvmH.MakeStaticCall(*validatorsAddress, getValidatorsFuncABI, "getValidators", []interface{}{}, &newValSet, maxGasForGetValidators, header, state)
 		return newValSet, err
