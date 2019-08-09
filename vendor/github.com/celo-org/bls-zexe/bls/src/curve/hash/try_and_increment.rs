d3f694f827f103d636e850c4d7bfecd97d7e6bba
Kobi Gurkan <kobigurk@gmail.com>
2019-08-09 17:26:27 +0300

    upgrades bls-zexe to correctly sample g2 points

--- vendor/github.com/celo-org/bls-zexe/bls/src/curve/hash/try_and_increment.rs
+++ vendor/github.com/celo-org/bls-zexe/bls/src/curve/hash/try_and_increment.rs
@@ -81,19 +81,21 @@ impl<'a, H: PRF> HashToG2 for TryAndIncrement<'a, H> {
         const NUM_TRIES: usize = 256;
         const EXPECTED_TOTAL_BITS: usize = 384*2;
         const LAST_BYTE_MASK: u8 = 1;
+        const GREATEST_MASK: u8 = 2;
 
         //round up to a multiple of 8
         let fp_bits = (((<P::Fp as PrimeField>::Params::MODULUS_BITS as f64)/8.0).ceil() as usize)*8;
-        let num_bits = 2 * fp_bits; //2*(Fq + EXTRA_BITS), generate 2 field elements with extra bits
+        let num_bits = 2 * fp_bits;
         assert_eq!(num_bits, EXPECTED_TOTAL_BITS);
         let message_hash = self.hasher.crh(message)?;
         let mut counter: [u8; 1] = [0; 1];
+        let hash_loop_time = timer_start!(|| "try_and_increment::hash_loop");
         for c in 0..NUM_TRIES {
             (&mut counter[..]).write_u8(c as u8)?;
             let hash = self
                 .hasher
                 .prf(key, domain, &[&counter, extra_data, message_hash.as_slice()].concat(), num_bits)?;
-            let possible_x = {
+            let (possible_x, greatest) = {
                 //zero out the last byte except the first bit, to get to a total of 377 bits
                 let mut possible_x_0_bytes = (&hash[..hash.len()/2]).to_vec();
                 let possible_x_0_bytes_len = possible_x_0_bytes.len();
@@ -104,14 +106,15 @@ impl<'a, H: PRF> HashToG2 for TryAndIncrement<'a, H> {
                 }
                 let mut possible_x_1_bytes = (&hash[hash.len() / 2..]).to_vec();
                 let possible_x_1_bytes_len = possible_x_1_bytes.len();
+                let greatest = (possible_x_1_bytes[possible_x_1_bytes_len - 1] & GREATEST_MASK) == GREATEST_MASK;
                 possible_x_1_bytes[possible_x_1_bytes_len - 1] &= LAST_BYTE_MASK;
                 let possible_x_1 = P::Fp::read(possible_x_1_bytes.as_slice())?;
                 if possible_x_1 == P::Fp::zero() {
                     continue;
                 }
-                Fp2::<P::Fp2Params>::new(possible_x_0, possible_x_1)
+                (Fp2::<P::Fp2Params>::new(possible_x_0, possible_x_1), greatest)
             };
-            match get_point_from_x::<P>(possible_x, true) {
+            match get_point_from_x::<P>(possible_x, greatest) {
                 None => continue,
                 Some(x) => {
                     debug!(
@@ -119,6 +122,7 @@ impl<'a, H: PRF> HashToG2 for TryAndIncrement<'a, H> {
                         hex::encode(message),
                         c
                     );
+                    timer_end!(hash_loop_time);
                     return Ok(cofactor::scale_by_cofactor_fuentes::<P>(
                         &x.into_projective(),
                     ));
