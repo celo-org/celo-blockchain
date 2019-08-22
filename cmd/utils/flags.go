4ae63d312018ce3a9b7448c6f0f2a3fb432b4691
Joshua Gutow <joshua@celo.org>
2019-08-22 10:36:44 -0700

    Small fix

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -971,7 +971,7 @@ func SetP2PConfig(ctx *cli.Context, cfg *p2p.Config) {
 			cfg.MaxPeers = lightPeers
 		}
 	}
-	if !(lightClient || lightServer) || cfg.MaxPeers == 0 {
+	if !(lightClient || lightServer) {
 		lightPeers = 0
 	}
 	ethPeers := cfg.MaxPeers - lightPeers
