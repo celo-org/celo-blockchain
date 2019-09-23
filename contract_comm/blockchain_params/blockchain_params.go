cd822ad79df3d2b8429f891a40fc39bb1dda44fd
Sami Mäkelä <mrsmkl@gmail.com>
2019-09-23 15:52:25 +0300

    comparing versions

--- contract_comm/blockchain_params/blockchain_params.go
+++ contract_comm/blockchain_params/blockchain_params.go
@@ -1,3 +1,19 @@
+// Copyright 2017 The Celo Authors
+// This file is part of the celo library.
+//
+// The celo library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The celo library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the celo library. If not, see <http://www.gnu.org/licenses/>.
+
 package blockchain_params
 
 import (
@@ -55,7 +71,12 @@ func CheckMinimumVersion(header *types.Header, state vm.StateDB) error {
 	}
 
 	log.Warn("Version", "version", minVersion)
-	_, err = version.NewVersion(minVersion)
+	v1, err := version.NewVersion(minVersion)
+	v2, err := version.NewVersion(params.Version)
 
-	return err
+	if v2.LessThan(v1) {
+		log.Crit("Client version older than required", "current", params.Version, "required", minVersion)
+	}
+
+	return nil
 }
