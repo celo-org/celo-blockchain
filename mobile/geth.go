6711822e61501585a031baa834ac644cb78af24c
Trevor Porter <trkporter@ucdavis.edu>
2019-09-23 10:21:09 -0700

    Change PeerDiscovery -> NoDiscovery

--- mobile/geth.go
+++ mobile/geth.go
@@ -86,8 +86,8 @@ type NodeConfig struct {
 	// WhisperEnabled specifies whether the node should run the Whisper protocol.
 	WhisperEnabled bool
 
-	// PeerDiscovery indicates whether the node should participate in p2p discovery
-	PeerDiscovery bool
+	// NoDiscovery indicates whether the node should not participate in p2p discovery
+	NoDiscovery bool
 
 	// Listening address of pprof server.
 	PprofAddress string
@@ -150,8 +150,8 @@ func NewNode(datadir string, config *NodeConfig) (stack *Node, _ error) {
 		UseLightweightKDF: config.UseLightweightKDF,
 		IPCPath:           "geth.ipc",
 		P2P: p2p.Config{
-			NoDiscovery:      !config.PeerDiscovery,
-			DiscoveryV5:      config.PeerDiscovery,
+			NoDiscovery:      config.NoDiscovery,
+			DiscoveryV5:      !config.NoDiscovery,
 			BootstrapNodesV5: config.BootstrapNodes.nodes,
 			ListenAddr:       ":0",
 			NAT:              nat.Any(),
