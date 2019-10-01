ec30e4bff9ad68a67bc20a257f87c94f2cfd5dc5
Sami Mäkelä <mrsmkl@gmail.com>
2019-10-01 17:24:00 +0300

    hmm, last change wasn't a good idea

--- contract_comm/gasprice_minimum/gasprice_minimum.go
+++ contract_comm/gasprice_minimum/gasprice_minimum.go
@@ -120,14 +120,14 @@ func GetGasPriceMinimum(currency *common.Address, header *types.Header, state vm
 	var currencyAddress *common.Address
 	var err error
 
-	if state == nil || header == nil {
-		return FallbackGasPriceMinimum, nil
-	}
-
 	if currency == nil {
 		currencyAddress, err = contract_comm.GetRegisteredAddress(params.GoldTokenRegistryId, header, state)
 
 		if err == errors.ErrSmartContractNotDeployed || err == errors.ErrRegistryContractNotDeployed {
+			return FallbackGasPriceMinimum, nil
+		}
+		if err == errors.ErrNoInternalEvmHandlerSingleton {
+			log.Error(err.Error())
 			return FallbackGasPriceMinimum, nil
 		}
 		if err != nil {
