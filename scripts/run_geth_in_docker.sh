ec9f7b6f7286edee7f13c2a238c6136d88855c9e
Kobi Gurkan <kobigurk@gmail.com>
2019-12-06 20:45:42 +0200

    scripts: docker should now exit using CTRL+c

--- scripts/run_geth_in_docker.sh
+++ scripts/run_geth_in_docker.sh
@@ -1,3 +1,10 @@
+# allow exiting using CTRL + C
+exit_func() {
+        echo "Exiting..."
+        exit 1
+}
+trap exit_func SIGTERM SIGINT
+
 # 150 is an arbitrary choice, which seems good enough
 stty cols 150
 sleep 1
