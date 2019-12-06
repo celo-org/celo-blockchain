a12395f6b7d7c0365c65f2829f81a9a46debb00c
Kobi Gurkan <kobigurk@gmail.com>
2019-12-06 14:53:18 +0200

    scripts: makes update_bls_zexe work

--- scripts/update_bls_zexe.sh
+++ scripts/update_bls_zexe.sh
@@ -4,4 +4,5 @@ BRANCH=${1:-master}
 SCRIPTS_DIR=`dirname $0`
 cd $SCRIPTS_DIR/..
 rm -r ./crypto/bls/bls-zexe
+git commit -am"bls-zexe: removes old version"
 git subtree add --prefix crypto/bls/bls-zexe https://github.com/celo-org/bls-zexe $BRANCH --squash
