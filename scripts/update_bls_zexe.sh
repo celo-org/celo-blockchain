1a3b73d04fcf0e0b6294cd01554628c17e81cf9e
Kobi Gurkan <kobigurk@gmail.com>
2019-12-06 14:49:26 +0200

    scripts: add update_bls_zexe.sh

---
+++ scripts/update_bls_zexe.sh
@@ -0,0 +1,5 @@
+BRANCH=$1 || master
+SCRIPTS_DIR=`dirname $0`
+cd $SCRIPTS_DIR/..
+rm -r ./crypto/bls/bls-zexe
+git subtree add --prefix crypto/bls/bls-zexe https://github.com/celo-org/bls-zexe $BRANCH --squash
