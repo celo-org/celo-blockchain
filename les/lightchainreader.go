4391ffd5fa6d96365848efdd7962e2088745090e
Connor McEwen <connor.mcewen@gmail.com>
2019-12-17 16:52:56 -0800

    Add les chain reader

---
+++ les/lightchainreader.go
@@ -0,0 +1,34 @@
+package les
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/light"
+	"github.com/ethereum/go-ethereum/params"
+)
+
+type LightChainReader struct {
+	config     *params.ChainConfig
+	blockchain *light.LightChain
+}
+
+// Config returns the chain configuration.
+func (lcr *LightChainReader) Config() *params.ChainConfig {
+	return lcr.config
+}
+
+func (lcr *LightChainReader) CurrentHeader() *types.Header {
+	return lcr.blockchain.CurrentHeader()
+}
+func (lcr *LightChainReader) GetHeaderByNumber(number uint64) *types.Header {
+	return lcr.blockchain.GetHeaderByNumber(number)
+}
+func (lcr *LightChainReader) GetHeaderByHash(hash common.Hash) *types.Header {
+	return lcr.blockchain.GetHeaderByHash(hash)
+}
+func (lcr *LightChainReader) GetHeader(hash common.Hash, number uint64) *types.Header {
+	return lcr.blockchain.GetHeader(hash, number)
+}
+func (lcr *LightChainReader) GetBlock(hash common.Hash, number uint64) *types.Block {
+	return nil
+}
