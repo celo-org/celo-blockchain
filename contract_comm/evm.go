f0294ba3858438ae5f10df996624fcd78b6a692f
Victor "Nate" Graf <victor@celo.org>
2020-01-09 13:53:10 -0800

    Add metrics timers to block finalization and evm execution (#802)
    * add metrics timers to block finalization and evm execution
    
    * add commit hash to branch builds
    
    * revise docker file build instructions to include hash

--- contract_comm/evm.go
+++ contract_comm/evm.go
@@ -19,6 +19,7 @@ package contract_comm
 import (
 	"math/big"
 	"reflect"
+	"time"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
@@ -27,11 +28,15 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/metrics"
 )
 
 var (
 	emptyMessage                = types.NewMessage(common.HexToAddress("0x0"), nil, 0, common.Big0, 0, common.Big0, nil, nil, common.Big0, []byte{}, false)
 	internalEvmHandlerSingleton *InternalEVMHandler
+
+	// Metrics timers to track the execution time of calls made from the system to core contracts.
+	systemCallTimers = make(map[string]metrics.Timer)
 )
 
 // An EVM handler to make calls to smart contracts from within geth
@@ -94,6 +99,15 @@ func createEVM(header *types.Header, state vm.StateDB) (*vm.EVM, error) {
 }
 
 func makeCallFromSystem(scAddress common.Address, abi abi.ABI, funcName string, args []interface{}, returnObj interface{}, gas uint64, value *big.Int, header *types.Header, state vm.StateDB, static bool) (uint64, error) {
+	// Record a metrics data point about execution time.
+	start := time.Now()
+	timer, ok := systemCallTimers[funcName]
+	if !ok {
+		timer = metrics.NewRegisteredTimer("contract_comm/systemcall/"+funcName, nil)
+		systemCallTimers[funcName] = timer
+	}
+	defer timer.UpdateSince(start)
+
 	vmevm, err := createEVM(header, state)
 	if err != nil {
 		return 0, err
@@ -107,7 +121,7 @@ func makeCallFromSystem(scAddress common.Address, abi abi.ABI, funcName string, 
 		gasLeft, err = vmevm.CallFromSystem(scAddress, abi, funcName, args, returnObj, gas, value)
 	}
 	if err != nil {
-		log.Error("Error when invoking evm function", "err", err, "funcName", funcName, "static", static, "address", scAddress, "args", args)
+		log.Error("Error when invoking evm function", "err", err, "funcName", funcName, "static", static, "address", scAddress, "args", args, "gas", gas, "gasLeft", gasLeft, "value", value)
 		return gasLeft, err
 	}
 
