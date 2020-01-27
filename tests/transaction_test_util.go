74364d2c00bce98eb6a22b7711cee505990d8af0
Mariano Cortesi <mariano@celo.org>
2020-01-27 14:19:16 -0300

    Fix compilation problems

--- tests/transaction_test_util.go
+++ tests/transaction_test_util.go
@@ -56,7 +56,7 @@ func (tt *TransactionTest) Run(config *params.ChainConfig) error {
 			return nil, nil, err
 		}
 		// Intrinsic gas
-		requiredGas, err := core.IntrinsicGas(tx.Data(), tx.To() == nil, nil, nil, nil)
+		requiredGas, err := core.IntrinsicGas(tx.Data(), tx.To() == nil, nil, nil, nil, false)
 		if err != nil {
 			return nil, nil, err
 		}
