d1add7964eb2ab1a47c65f0239cea3a99a24abc9
Kevin Jue <kevin@celo.org>
2020-01-29 13:42:26 -0800

    fixed unit test for istanbul backend msg caches

--- consensus/istanbul/backend/handler_test.go
+++ consensus/istanbul/backend/handler_test.go
@@ -24,7 +24,7 @@ import (
 	"github.com/ethereum/go-ethereum/p2p"
 	"github.com/ethereum/go-ethereum/p2p/enode"
 	"github.com/ethereum/go-ethereum/rlp"
-	// lru "github.com/hashicorp/golang-lru"
+	lru "github.com/hashicorp/golang-lru"
 )
 
 type MockPeer struct{}
@@ -55,39 +55,82 @@ func TestIstanbulMessage(t *testing.T) {
 	}
 }
 
-func TestRecentMessageCachesNonAnnounce(t *testing.T) {
-	_, backend := newBlockChain(1, true)
-
-	// generate a msg that is not an Announce
-	data := []byte("data1")
-	hash := istanbul.RLPHash(data)
-	msg := makeMsg(istanbulAnnounceMsg, data)
-	addr := common.BytesToAddress([]byte("address"))
-
-	// 1. this message should not be in cache
-	// for peers
-	if _, ok := backend.peerRecentMessages.Get(addr); ok {
-		t.Fatalf("the cache of messages for this peer should be nil")
+func TestRecentMessageCaches(t *testing.T) {
+	// Define the various voting scenarios to test
+	tests := []struct {
+		ethMsgCode  uint64
+		shouldCache bool
+	}{
+		{
+			ethMsgCode:  istanbulConsensusMsg,
+			shouldCache: false,
+		},
+		{
+			ethMsgCode:  istanbulAnnounceMsg,
+			shouldCache: true,
+		},
+		{
+			ethMsgCode:  istanbulValEnodesShareMsg,
+			shouldCache: false,
+		},
+		{
+			ethMsgCode:  istanbulFwdMsg,
+			shouldCache: false,
+		},
+		{
+			ethMsgCode:  istanbulGetAnnouncesMsg,
+			shouldCache: false,
+		},
+		{
+			ethMsgCode:  istanbulGetAnnounceVersionsMsg,
+			shouldCache: false,
+		},
+		{
+			ethMsgCode:  istanbulAnnounceVersionsMsg,
+			shouldCache: false,
+		},
 	}
 
-	// for self
-	if _, ok := backend.selfRecentMessages.Get(hash); ok {
-		t.Fatalf("the cache of messages should be nil")
-	}
+	for _, tt := range tests {
+		_, backend := newBlockChain(1, true)
 
-	// 2. this message should NOT be in cache after we handle it
-	_, err := backend.HandleMsg(addr, msg, &MockPeer{})
-	if err != nil {
-		t.Fatalf("handle message failed: %v", err)
-	}
+		// generate a msg that is not an Announce
+		data := []byte("data1")
+		hash := istanbul.RLPHash(data)
+		msg := makeMsg(tt.ethMsgCode, data)
+		addr := common.BytesToAddress([]byte("address"))
 
-	// for peers
-	if _, ok := backend.peerRecentMessages.Get(addr); ok {
-		t.Fatalf("the cache of messages for this peer should be nil")
-	}
-	// for self
-	if _, ok := backend.selfRecentMessages.Get(hash); ok {
-		t.Fatalf("the cache of messages must be nil")
+		// 1. this message should not be in cache
+		// for peers
+		if _, ok := backend.peerRecentMessages.Get(addr); ok {
+			t.Fatalf("the cache of messages for this peer should be nil")
+		}
+
+		// for self
+		if _, ok := backend.selfRecentMessages.Get(hash); ok {
+			t.Fatalf("the cache of messages should be nil")
+		}
+
+		// 2. this message should be in cache only when ethMsgCode == istanbulAnnounceMsg
+		_, err := backend.HandleMsg(addr, msg, &MockPeer{})
+		if err != nil {
+			t.Fatalf("handle message failed: %v", err)
+		}
+
+		// for peers
+		if ms, ok := backend.peerRecentMessages.Get(addr); tt.shouldCache != ok {
+			t.Fatalf("the cache of messages for this peer should be nil")
+		} else if tt.shouldCache {
+			if m, ok := ms.(*lru.ARCCache); !ok {
+				t.Fatalf("the cache of messages for this peer cannot be casted")
+			} else if _, ok := m.Get(hash); !ok {
+				t.Fatalf("the cache of messages for this peer cannot be found")
+			}
+		}
+		// for self
+		if _, ok := backend.selfRecentMessages.Get(hash); tt.shouldCache != ok {
+			t.Fatalf("the cache of messages must be nil")
+		}
 	}
 }
 
