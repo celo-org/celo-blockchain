c90ec04bc7800f7a6c9db28935ff029e8f3fb701
Alec Schaefer <25732362+alecps@users.noreply.github.com>
2020-03-03 14:38:58 -0800

    Filters gold transfers from tx pool if transfers are frozen (#890)
    * adds transfer_whitelist and freezer to contract_comm
    
    * starts to add check
    
    * Adds check in validateTx
    
    * fixes import statements
    
    * adds a simple test
    
    * saving progress
    
    * fixes error
    
    * saving progress
    
    * fixes small error in node_test
    
    * removes unit tests
    
    * adds check for frozen gold transfers to light/txpool.go
    
    * fixes error
    
    * bumps MaxGasForGetTransferWhitelist

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -162,6 +162,7 @@ var (
 	ElectionRegistryId             = makeRegistryId("Election")
 	EpochRewardsRegistryId         = makeRegistryId("EpochRewards")
 	FeeCurrencyWhitelistRegistryId = makeRegistryId("FeeCurrencyWhitelist")
+	FreezerRegistryId              = makeRegistryId("Freezer")
 	GasPriceMinimumRegistryId      = makeRegistryId("GasPriceMinimum")
 	GoldTokenRegistryId            = makeRegistryId("GoldToken")
 	GovernanceRegistryId           = makeRegistryId("Governance")
@@ -170,6 +171,7 @@ var (
 	ReserveRegistryId              = makeRegistryId("Reserve")
 	SortedOraclesRegistryId        = makeRegistryId("SortedOracles")
 	StableTokenRegistryId          = makeRegistryId("StableToken")
+	TransferWhitelistRegistryId    = makeRegistryId("TransferWhitelist")
 	ValidatorsRegistryId           = makeRegistryId("Validators")
 
 	// Function is "getOrComputeTobinTax()"
@@ -219,6 +221,7 @@ const (
 	MaxGasForGetWhiteList                          uint64 = 200000
 	MaxGasForGetTransferWhitelist                  uint64 = 2000000
 	MaxGasForIncreaseSupply                        uint64 = 50 * 1000
+	MaxGasForIsFrozen                              uint64 = 20000
 	MaxGasForMedianRate                            uint64 = 100000
 	MaxGasForReadBlockchainParameter               uint64 = 20000
 	MaxGasForRevealAndCommit                       uint64 = 2000000
