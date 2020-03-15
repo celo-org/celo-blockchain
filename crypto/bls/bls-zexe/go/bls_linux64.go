a90054512332cf1319f1e75d1eb5913b11136b33
Kobi Gurkan <kobigurk@gmail.com>
2020-03-15 20:21:15 +0200

    bls-zexe: removes old version

--- crypto/bls/bls-zexe/go/bls_linux64.go
+++
@@ -1,8 +0,0 @@
-// +build linux,amd64
-
-package bls
-
-/*
-#cgo LDFLAGS: -L../target/x86_64-unknown-linux-gnu/release -lepoch_snark -ldl -lm
-*/
-import "C"
--- crypto/bls/bls-zexe/go/bls_windows64.go
+++
@@ -1,8 +0,0 @@
-// +build windows,amd64
-
-package bls
-
-/*
-#cgo LDFLAGS: -L../target/x86_64-pc-windows-gnu/release -lepoch_snark -lm -lws2_32 -luserenv -lunwind
-*/
-import "C"
