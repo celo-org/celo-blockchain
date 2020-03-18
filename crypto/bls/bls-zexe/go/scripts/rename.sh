8cb6c556dca9cf034db37e7bd2664e8df5508699
Kobi Gurkan <kobigurk@gmail.com>
2020-03-18 08:28:24 +0200

    bls-zexe: removes old version

--- crypto/bls/bls-zexe/go/scripts/rename.sh
+++
@@ -1,8 +0,0 @@
-cp `dirname $0`/../bls/bls_* .
-for name in bls_*
-do
-    newname=$1"$(echo "$name" | cut -c4-)"
-    mv "$name" "$newname"
-    sed -i "" "s/package bls/package $1/g" $newname
-done
-
