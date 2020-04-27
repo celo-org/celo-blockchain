172d08664ac752a43ff53fd4fa15475aafdbdc85
Kobi Gurkan <kobigurk@gmail.com>
2020-04-27 21:37:31 +0300

    moves token data to a separate file and adds a comment on ERC20 transfer prefix

--- accounts/usbwallet/ledger/tokens.go
+++ accounts/usbwallet/ledger/tokens.go
@@ -10,8 +10,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-const TokensBlob = "AAAAaARjVVNEdl3oFoRYYedaJfyhIrtomLixKCoAAAASAACk7DBFAiEApwQFHNBKXp+V2jq8BMD2y/5AwC9bhPQ2H4hT/vMl/B4CIFalOVtBFGREUKMU/F5vDlJLeQrTn6GQeDertpB2FpMvAAAAZwRjR0xERx7ON1DaI3+TuOM5xTaYm4l4pDgAAAASAACk7DBEAiAtUE03OVEDTuAgMR8CeUiXVB7Uqa+4gQHaZtXsU0tjMQIgTsY5//96mGMOtyoQFgjDrh3kztxlUO90MJOLElo6baQ=" // #nosec
-
 var ErrCouldNotFindToken = errors.New("could not find token")
 var ErrNotAnERC20Transfer = errors.New("not an ERC20 transfer")
 
@@ -94,6 +92,7 @@ func IsERC20Transfer(data []byte) bool {
 		return false
 	}
 
+	// 0xa9059cbb is the ERC20 transfer function signature
 	if !bytes.Equal(data[:4], []byte{0xa9, 0x05, 0x9c, 0xbb}) {
 		return false
 	}
