4bc3ab59821cf80a83de5995dfecc64407bbe667
Victor "Nate" Graf <victor@celo.org>
2020-07-15 17:53:39 -0700

    Use correct address to select BLS key when ECDSA key is different (#1103)
    ### Description
    
    Currently, when the ECDSA and BLS keys are set differently (e.g. through the `--etherbase` and `--blsbase` flags, the ECDSA address is used to issue signing requests to the wallet, which results in an unknown account error. This PR adds the BLS address as a field to the engine and uses it for signing requests.
    
    ### Tested
    
    Currently untested
    
    ### Backwards compatibility
    
    100%

--- consensus/istanbul/backend/val_enodes_share_test.go
+++ consensus/istanbul/backend/val_enodes_share_test.go
@@ -31,7 +31,7 @@ func TestHandleValEnodeShareMsg(t *testing.T) {
 		t.Errorf("error %v", err)
 	}
 
-	b.Authorize(getAddress(), &privateKey.PublicKey, decryptFn, SignFn(nil), SignBLSFn(nil))
+	b.Authorize(getAddress(), getAddress(), &privateKey.PublicKey, decryptFn, SignFn(nil), SignBLSFn(nil))
 
 	// Set the backend's proxied validator address to itself
 	b.config.ProxiedValidatorAddress = senderAddress
