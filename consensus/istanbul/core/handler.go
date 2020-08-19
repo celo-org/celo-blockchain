e10be6d45ac461496ee4c56c6524f745f42a52c1
Joshua Gutow <joshua@clabs.co>
2020-08-19 17:04:38 -0400

    Another hook to keep the core up to date

--- consensus/istanbul/core/handler.go
+++ consensus/istanbul/core/handler.go
@@ -153,6 +153,14 @@ func (c *core) handleEvents() {
 			if !ok {
 				return
 			}
+			// Add another hook on timeout events
+			if c.backend.IsElectedValidator() && !c.backend.IsValidating() {
+				headBlock := c.backend.GetCurrentHeadBlock()
+				if headBlock.Number().Cmp(c.current.Sequence()) >= 0 {
+					go c.backend.EventMux().Post(istanbul.FinalCommittedEvent{})
+				}
+				return
+			}
 			switch ev := event.Data.(type) {
 			case timeoutAndMoveToNextRoundEvent:
 				if err := c.handleTimeoutAndMoveToNextRound(ev.view); err != nil {
