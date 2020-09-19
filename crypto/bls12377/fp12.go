eb6f96dbe9dd05b3a77694fbb7d006e2d20add66
kilic <kiliconu@itu.edu.tr>
2020-09-19 19:01:57 +0300

    use lazy operations in extensions
    use assigned operations in fp2
    
    add math comments

--- crypto/bls12377/fp12.go
+++ crypto/bls12377/fp12.go
@@ -81,48 +81,61 @@ func (e *fp12) one() *fe12 {
 }
 
 func (e *fp12) add(c, a, b *fe12) {
+	// c0 = a0 + b0
+	// c1 = a1 + b1
 	fp6 := e.fp6
 	fp6.add(&c[0], &a[0], &b[0])
 	fp6.add(&c[1], &a[1], &b[1])
-
 }
 
 func (e *fp12) double(c, a *fe12) {
+	// c0 = 2a0
+	// c1 = 2a1
 	fp6 := e.fp6
 	fp6.double(&c[0], &a[0])
 	fp6.double(&c[1], &a[1])
 }
 
 func (e *fp12) sub(c, a, b *fe12) {
+	// c0 = a0 - b0
+	// c1 = a1 - b1
 	fp6 := e.fp6
 	fp6.sub(&c[0], &a[0], &b[0])
 	fp6.sub(&c[1], &a[1], &b[1])
-
 }
 
 func (e *fp12) neg(c, a *fe12) {
+	// c0 = -a0
+	// c1 = -a1
 	fp6 := e.fp6
 	fp6.neg(&c[0], &a[0])
 	fp6.neg(&c[1], &a[1])
 }
 
 func (e *fp12) conjugate(c, a *fe12) {
+	// c0 = a0
+	// c1 = -a1
 	fp6 := e.fp6
 	c[0].set(&a[0])
 	fp6.neg(&c[1], &a[1])
 }
 
 func (e *fp12) square(c, a *fe12) {
+	// Multiplication and Squaring on Pairing-Friendly Fields
+	// Complex squaring algorithm
+	// https://eprint.iacr.org/2006/471
+
 	fp6, t := e.fp6, e.t6
-	fp6.add(t[0], &a[0], &a[1])
-	fp6.mul(t[2], &a[0], &a[1])
-	fp6.mulByNonResidue(t[1], &a[1])
-	fp6.add(t[1], t[1], &a[0])
-	fp6.mulByNonResidue(t[3], t[2])
-	fp6.mul(t[0], t[0], t[1])
-	fp6.sub(t[0], t[0], t[2])
-	fp6.sub(&c[0], t[0], t[3])
-	fp6.double(&c[1], t[2])
+
+	fp6.ladd(t[0], &a[0], &a[1])     // a0 + a1
+	fp6.mul(t[2], &a[0], &a[1])      // v0 = a0a1
+	fp6.mulByNonResidue(t[1], &a[1]) // βa1
+	fp6.ladd(t[1], t[1], &a[0])      // a0 + βa1
+	fp6.double(&c[1], t[2])          // c1 = 2v0
+	fp6.mulByNonResidue(t[3], t[2])  // βa0a1
+	fp6.mul(t[0], t[0], t[1])        // (a0 + a1)(a0 + βa1)
+	fp6.sub(t[0], t[0], t[2])        // (a0 + a1)(a0 + βa1) - v0
+	fp6.sub(&c[0], t[0], t[3])       // c0 = (a0 + a1)(a0 + βa1) - v0
 }
 
 func (e *fp12) cyclotomicSquare(c, a *fe12) {
@@ -152,30 +165,28 @@ func (e *fp12) cyclotomicSquare(c, a *fe12) {
 }
 
 func (e *fp12) mul(c, a, b *fe12) {
-	t, fp6 := e.t6, e.fp6
 	// Guide to Pairing Based Cryptography
-	// 5. Arithmetic of Finite Fields
 	// Algorithm 5.16
-	//
-	// c0 = a0b0 - 5a1b1
-	// c1 = (a0 + a1)(b0 + b1) - (a0b0 + a1b1)
-	fp6.mul(t[1], &a[0], &b[0])     // a0b0
-	fp6.mul(t[2], &a[1], &b[1])     // a1b1
-	fp6.add(t[0], t[1], t[2])       // a0b0 + a1b1
-	fp6.mulByNonResidue(t[2], t[2]) // αa1b1
-	fp6.add(t[3], t[1], t[2])       // c0 = a0b0 + αa1b1
-	fp6.add(t[1], &a[0], &a[1])
-	fp6.add(t[2], &b[0], &b[1])
-	fp6.mul(t[1], t[1], t[2])
-	c[0].set(t[3])
-	fp6.sub(&c[1], t[1], t[0])
+
+	t, fp6 := e.t6, e.fp6
+
+	fp6.mul(t[1], &a[0], &b[0])     // v0 = a0b0
+	fp6.mul(t[2], &a[1], &b[1])     // v1 = a1b1
+	fp6.ladd(t[0], &a[0], &a[1])    // a0 + a1
+	fp6.ladd(t[3], &b[0], &b[1])    // b0 + b1
+	fp6.mul(t[0], t[0], t[3])       // (a0 + a1)(b0 + b1)
+	fp6.sub(t[0], t[0], t[1])       // (a0 + a1)(b0 + b1) - v0
+	fp6.sub(&c[1], t[0], t[2])      // c1 = (a0 + a1)(b0 + b1) - v0 - v1
+	fp6.mulByNonResidue(t[2], t[2]) // βv1
+	fp6.add(&c[0], t[1], t[2])      // c0 = v0 + 4v1
 }
 
 func (fp12 *fp12) mul034(a *fe12, b0, b3, b4 *fe2) {
-	fp2, fp6, t := fp12.fp2(), fp12.fp6, fp12.t6
 	// Software Implementation of the Optimal Ate Pairing over Barreto–Naehrig Curves
 	// Algorithm 21
 	// https://eprint.iacr.org/2010/354.pdf
+
+	fp2, fp6, t := fp12.fp2(), fp12.fp6, fp12.t6
 
 	fp6.mul0(t[0], &a[0], b0)      // t0 = a0b0
 	fp6.mul01(t[1], &a[1], b3, b4) // t1 = a1b1
@@ -202,15 +213,19 @@ func (e *fp12) fp4Square(c0, c1, a0, a1 *fe2) {
 }
 
 func (e *fp12) inverse(c, a *fe12) {
+	// Guide to Pairing Based Cryptography
+	// Algorithm 5.16
+
 	fp6, t := e.fp6, e.t6
-	fp6.square(t[0], &a[0])
-	fp6.square(t[1], &a[1])
-	fp6.mulByNonResidue(t[1], t[1])
-	fp6.sub(t[1], t[0], t[1])
-	fp6.inverse(t[0], t[1])
-	fp6.mul(&c[0], &a[0], t[0])
-	fp6.mul(t[0], t[0], &a[1])
-	fp6.neg(&c[1], t[0])
+
+	fp6.square(t[0], &a[0])         // a0^2
+	fp6.square(t[1], &a[1])         // a1^2
+	fp6.mulByNonResidue(t[1], t[1]) // βa1^2
+	fp6.sub(t[1], t[0], t[1])       // v = (a0^2 - a1^2)
+	fp6.inverse(t[0], t[1])         // v = v^-1
+	fp6.mul(&c[0], &a[0], t[0])     // c0 = a0v
+	fp6.mul(t[0], t[0], &a[1])      //
+	fp6.neg(&c[1], t[0])            // c1 = -a1v
 }
 
 func (e *fp12) exp(c, a *fe12, s *big.Int) {
