0f1e8c434dd5dfcd6f4603f7166d4c8a32e3d9a9
kilic <kiliconu@itu.edu.tr>
2020-09-19 17:47:44 +0300

    add size constants
    better comments for constants

--- crypto/bls12377/bls12_377.go
+++ crypto/bls12377/bls12_377.go
@@ -1,11 +1,16 @@
 package bls12377
 
-/*
-	Field Constants
-*/
+const N_LIMBS = 6
+const FE_BYTE_SIZE = 48
+const FE_BIT_SIZE = 377
+const SIX_WORD_BIT_SIZE = 384
+const SCALAR_FIELD_BIT_SIZE = 253
 
-// Base field modulus
-// p = 0x01ae3a4617c510eac63b05c06ca1493b1a22d9f300f5138f1ef3622fba094800170b5d44300000008508c00000000001
+/*
+ Field
+ p = 0x01ae3a4617c510eac63b05c06ca1493b1a22d9f300f5138f1ef3622fba094800170b5d44300000008508c00000000001
+ r = 2^384
+*/
 
 // modulus = p
 var modulus = fe{0x8508c00000000001, 0x170b5d4430000000, 0x1ef3622fba094800, 0x1a22d9f300f5138f, 0xc63b05c06ca1493b, 0x01ae3a4617c510ea}
@@ -13,13 +18,13 @@ var modulus = fe{0x8508c00000000001, 0x170b5d4430000000, 0x1ef3622fba094800, 0x1
 // -p^(-1) mod 2^64
 var inp uint64 = 0x8508bfffffffffff
 
-// r mod p
+// r1 = r mod p
 var r1 = &fe{0x02cdffffffffff68, 0x51409f837fffffb1, 0x9f7db3a98a7d3ff2, 0x7b4e97b76e7c6305, 0x4cf495bf803c84e8, 0x008d6661e2fdf49a}
 
-// r mod p
+// one = r mod p
 var one = r1
 
-// 0
+// zer0 = 0
 var zero = &fe{}
 
 // r^2 mod p
@@ -27,54 +32,50 @@ var r2 = &fe{
 	0xb786686c9400cd22, 0x0329fcaab00431b1, 0x22a5f11162d6b46d, 0xbfdf7d03827dc3ac, 0x837e92f041790bf9, 0x006dfccb1e914b88,
 }
 
-// - r mod p
+// negativeOne = -r mod p
 var negativeOne = &fe{0x823ac00000000099, 0xc5cabdc0b000004f, 0x7f75ae862f8c080d, 0x9ed4423b9278b089, 0x79467000ec64c452, 0x0120d3e434c71c50}
 
-// -1 + 0 * u
-var negativeOne2 = &fe2{
-	fe{0x823ac00000000099, 0xc5cabdc0b000004f, 0x7f75ae862f8c080d, 0x9ed4423b9278b089, 0x79467000ec64c452, 0x0120d3e434c71c50},
-	fe{0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000, 0x0000000000000000},
-}
-
-// 2 ^ (-1)
+// twoInv = 2 ^ (-1)
 var twoInv = &fe{0x8166ffffffffffb4, 0x28a04fc1bfffffd8, 0xcfbed9d4c53e9ff9, 0x3da74bdbb73e3182, 0x267a4adfc01e4274, 0x0046b330f17efa4d}
 
-// (p - 1) / 2
+// pMinus1Over2 = (p - 1) / 2
 var pMinus1Over2 = bigFromHex("0x00d71d230be28875631d82e03650a49d8d116cf9807a89c78f79b117dd04a4000b85aea2180000004284600000000000")
 
-// p >> 46
-var s = bigFromHex("0x06b8e9185f1443ab18ec1701b28524ec688b67cc03d44e3c7bcd88bee82520005c2d7510c00000021423")
+// s = p >> 46
+// var s = bigFromHex("0x06b8e9185f1443ab18ec1701b28524ec688b67cc03d44e3c7bcd88bee82520005c2d7510c00000021423")
 
-// ((p >> 46) - 1) / 2
+// sSqrt = ((p >> 46) - 1) / 2
 var sSqrt = bigFromHex("0x035c748c2f8a21d58c760b80d94292763445b3e601ea271e3de6c45f741290002e16ba88600000010a11")
 
-// -5
+// nonResidue1 = -5
 var nonResidue1 = &fe{0xfc0b8000000002fa, 0x97d39cf6e000018b, 0x2072420fbfa05044, 0xcbbcbd50d97c3802, 0x0baf1ec35813f9eb, 0x009974a2c0945ad2}
 
-// (0 + 1 * u)
+// nonResidue2 = (0 + 1 * u)
 var nonResidue2 = &fe2{
 	fe{0, 0, 0, 0, 0, 0},
 	fe{0x02cdffffffffff68, 0x51409f837fffffb1, 0x9f7db3a98a7d3ff2, 0x7b4e97b76e7c6305, 0x4cf495bf803c84e8, 0x008d6661e2fdf49a},
 }
 
-// nonResidue ^ s
+// sNonResidue = nonResidue ^ s
 var sNonResidue = &fe{0x1c104955744e6e0f, 0xf1bd15c3898dd1af, 0x76da78169a7f3950, 0xee086c1fe367c337, 0xf95564f4cbc1b61f, 0x00f3c1414ef58c54}
 
 /*
-	Curve Constants
+	Curve
 */
 
+// Group order
+var q = bigFromHex("0x12ab655e9a2ca55660b44d1e5c37b00159aa76fed00000010a11800000000001")
+
 // b coefficient for G1
+// b = 1
 var b = &fe{0x02cdffffffffff68, 0x51409f837fffffb1, 0x9f7db3a98a7d3ff2, 0x7b4e97b76e7c6305, 0x4cf495bf803c84e8, 0x008d6661e2fdf49a}
 
 // b coefficient for G2
+// b2 = (0, 0x10222f6db0fd6f343bd03737460c589dc7b4f91cd5fd889129207b63c6bf8000dd39e5c1ccccccd1c9ed9999999999a)
 var b2 = &fe2{
 	fe{0, 0, 0, 0, 0, 0},
 	fe{0x8072266666666685, 0x8df55926899999a9, 0x7fe4561ad64f34cf, 0xb95da6d8b6e4f01b, 0x4b747cccfc142743, 0x0039c3fa70f49f43},
 }
-
-// Group order
-var q = bigFromHex("0x12ab655e9a2ca55660b44d1e5c37b00159aa76fed00000010a11800000000001")
 
 // G1 cofactor
 var cofactorG1 = bigFromHex("0x170b5d44300000000000000000000000")
@@ -83,6 +84,8 @@ var cofactorG1 = bigFromHex("0x170b5d44300000000000000000000000")
 var cofactorG2 = bigFromHex("0x26ba558ae9562addd88d99a6f6a829fbb36b00e1dcc40c8c505634fae2e189d693e8c36676bd09a0f3622fba094800452217cc900000000000000000000001")
 
 // G1 generator
+// x = 0x008848defe740a67c8fc6225bf87ff5485951e2caa9d41bb188282c8bd37cb5cd5481512ffcd394eeab9b16eb21be9ef
+// y = 0x1914a69c5102eff1f674f5d30afeec4bd7fb348ca3e52d96d182ad44fb82305c2fe3d3634a9591afd82de55559c8ea6
 var g1One = PointG1{
 	fe{0x260f33b9772451f4, 0xc54dd773169d5658, 0x5c1551c469a510dd, 0x761662e4425e1698, 0xc97d78cc6f065272, 0x00a41206b361fd4d},
 	fe{0x8193961fb8cb81f3, 0x00638d4c5f44adb8, 0xfafaf3dad4daf54a, 0xc27849e2d655cd18, 0x2ec3ddb401d52814, 0x007da93326303c71},
@@ -92,6 +95,10 @@ var g1One = PointG1{
 var G1One = g1One
 
 // G2 generator
+// x0 = 0x018480be71c785fec89630a2a3841d01c565f071203e50317ea501f557db6b9b71889f52bb53540274e3e48f7c005196
+// x1 = 0x00ea6040e700403170dc5a51b1b140d5532777ee6651cecbe7223ece0799c9de5cf89984bff76fe6b26bfefa6ea16afe
+// y0 = 0x00690d665d446f7bd960736bcbb2efb4de03ed7274b49a58e458c282f832d204f2cf88886d8c7c2ef094094409fd4ddf
+// y1 = 0x00f8169fd28355189e549da3151a70aa61ef11ac3d591bf12463b01acee304c24279b83f5e52270bd9a1cdd185eb8f93
 var g2One = PointG2{
 	fe2{
 		fe{0x68904082f268725b, 0x668f2ea74f45328b, 0xebca7a65802be84f, 0x1e1850f4c1ada3e6, 0x830dc22d588ef1e9, 0x01862a81767c0982},
