e00311d860d5c972e49961bbad615bddc31a8a29
kilic <kiliconu@itu.edu.tr>
2020-09-19 19:01:57 +0300

    use lazy operations in extensions
    use assigned operations in fp2
    
    add math comments

--- crypto/bls12377/fp2.go
+++ crypto/bls12377/fp2.go
@@ -117,23 +117,21 @@ func (e *fp2) conjugate(c, a *fe2) {
 }
 
 func (e *fp2) mul(c, a, b *fe2) {
-	t := e.t
 	// Guide to Pairing Based Cryptography
-	// 5. Arithmetic of Finite Fields
 	// Algorithm 5.16
-	//
-	// c0 = a0b0 - 5a1b1
-	// c1 = (a0 + a1)(b0 + b1) - (a0b0 + a1b1)
-	mul(t[1], &a[0], &b[0]) // v0 = a0b0
-	mul(t[2], &a[1], &b[1]) // v1 = a1b1
-	add(t[0], t[1], t[2])   // v0 + v1
-	double(t[3], t[2])      // 2a1b1
-	doubleAssign(t[3])      // 4a1b1
-	addAssign(t[2], t[3])   // 5(a1 + b1)
-	sub(t[3], t[1], t[2])   // a0b0 - 5(a1 + b1)
-	add(t[1], &a[0], &a[1]) // a0 + a1
-	add(t[2], &b[0], &b[1]) // b0 + b1
-	mul(t[1], t[1], t[2])   // (a0 + a1)(b0 + b1)
+
+	t := e.t
+
+	mul(t[1], &a[0], &b[0])  // a0b0
+	mul(t[2], &a[1], &b[1])  // a1b1
+	add(t[0], t[1], t[2])    // v0 + v1
+	double(t[3], t[2])       // 2a1b1
+	doubleAssign(t[3])       // 4a1b1
+	addAssign(t[2], t[3])    // 5(a1 + b1)
+	sub(t[3], t[1], t[2])    // a0b0 - 5(a1 + b1)
+	ladd(t[1], &a[0], &a[1]) // a0 + a1
+	ladd(t[2], &b[0], &b[1]) // b0 + b1
+	mul(t[1], t[1], t[2])    // (a0 + a1)(b0 + b1)
 	c[0].set(t[3])
 	sub(&c[1], t[1], t[0]) // (a0 + a1)(b0 + b1) - (a0b0 + a1b1)
 }
@@ -144,29 +142,29 @@ func (e *fp2) mul0(c, a *fe2, b *fe) {
 }
 
 func (e *fp2) square(c, a *fe2) {
-	t := e.t
 	// Guide to Pairing Based Cryptography
-	// 5. Arithmetic of Finite Fields
 	// Algorithm 5.16
-	//
-	// c0 = (a0 - a1)(a0 + 5a1) - 4a1a0
-	// c1 = 2a0a1
+
+	t := e.t
+
 	sub(t[0], &a[0], &a[1]) // (a0 - a1)
-	double(t[1], &a[1])     // 2a1
+	ldouble(t[1], &a[1])    // 2a1
 	mul(t[2], t[1], &a[0])  // 2a0a1
 	c[1].set(t[2])
 	double(t[3], t[1])     // 4a1
 	addAssign(t[1], t[3])  // 6a1
 	doubleAssign(t[2])     // 4a0a1
-	addAssign(t[1], t[0])  // (a0 + 5a1)
+	laddAssign(t[1], t[0]) // (a0 + 5a1)
 	mul(t[0], t[0], t[1])  // (a0 - a1)(a0 - 5a1)
 	sub(&c[0], t[0], t[2]) // (a0 - a1)(a0 - 5a1) - 4a1a0
 }
 
 func (e *fp2) mulByNonResidue(c, a *fe2) {
+	// c0 = -5a1
+	// c1 = a0
+
 	t := e.t
-	// a = a0 + a1 * v
-	// c = -5a1 + a0 * v
+
 	t[0].set(&a[0])
 	neg(t[1], &a[1])
 	doubleAssign(t[1])
@@ -176,19 +174,21 @@ func (e *fp2) mulByNonResidue(c, a *fe2) {
 }
 
 func (e *fp2) inverse(c, a *fe2) {
+	// Guide to Pairing Based Cryptography
+	// Algorithm 5.16
+
 	t := e.t
-	// c0 = a0 * (a0^2 - β * a1^2)^-1
-	// c1 = a1 * (a0^2 - β * a1^2)^-1
-	square(t[0], &a[0])
-	square(t[1], &a[1])
-	double(t[2], t[1])
-	doubleAssign(t[2])
-	addAssign(t[1], t[2])
-	addAssign(t[0], t[1])
-	inverse(t[0], t[0])
-	mul(&c[0], &a[0], t[0])
-	mul(t[0], t[0], &a[1])
-	neg(&c[1], t[0])
+
+	square(t[0], &a[0])     // a0^2
+	square(t[1], &a[1])     // a1^2
+	double(t[2], t[1])      // 2a1^2
+	doubleAssign(t[2])      // 4a1^2
+	addAssign(t[1], t[2])   // 5a1^2
+	addAssign(t[0], t[1])   // a0^2 + 5a1^2
+	inverse(t[0], t[0])     // (a0^2 + 5a1^2)^-1
+	mul(&c[0], &a[0], t[0]) // a0((a0^2 + 5a1^2)^-1)
+	mul(t[0], t[0], &a[1])  // a1((a0^2 + 5a1^2)^-1)
+	neg(&c[1], t[0])        // -a1((a0^2 + 5a1^2)^-1)
 }
 
 func (e *fp2) exp(c, a *fe2, s *big.Int) {
