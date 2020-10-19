f420c2954b491b1286a221d04b5e6dc82cefdbf8
kilic <kiliconu@itu.edu.tr>
2020-10-19 13:41:22 +0300

    fix EIP name

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -1087,7 +1087,7 @@ var (
 	errBLS12377G2PointSubgroup    = errors.New("g2 point is not on correct subgroup")
 )
 
-// bls12377G1Add implements EIP-2537 G1Add precompile.
+// bls12377G1Add implements EIP-2539 G1Add precompile.
 type bls12377G1Add struct{}
 
 // RequiredGas returns the gas required to execute the pre-compiled contract.
@@ -1096,7 +1096,7 @@ func (c *bls12377G1Add) RequiredGas(input []byte) uint64 {
 }
 
 func (c *bls12377G1Add) Run(input []byte, caller common.Address, evm *EVM, gas uint64) ([]byte, uint64, error) {
-	// Implements EIP-2537 G1Add precompile.
+	// Implements EIP-2539 G1Add precompile.
 	// > G1 addition call expects `256` bytes as an input that is interpreted as byte concatenation of two G1 points (`128` bytes each).
 	// > Output is an encoding of addition operation result - single G1 point (`128` bytes).
 	if len(input) != 256 {
@@ -1125,7 +1125,7 @@ func (c *bls12377G1Add) Run(input []byte, caller common.Address, evm *EVM, gas u
 	return g.EncodePoint(r), gas, nil
 }
 
-// bls12377G1Mul implements EIP-2537 G1Mul precompile.
+// bls12377G1Mul implements EIP-2539 G1Mul precompile.
 type bls12377G1Mul struct{}
 
 // RequiredGas returns the gas required to execute the pre-compiled contract.
@@ -1134,7 +1134,7 @@ func (c *bls12377G1Mul) RequiredGas(input []byte) uint64 {
 }
 
 func (c *bls12377G1Mul) Run(input []byte, caller common.Address, evm *EVM, gas uint64) ([]byte, uint64, error) {
-	// Implements EIP-2537 G1Mul precompile.
+	// Implements EIP-2539 G1Mul precompile.
 	// > G1 multiplication call expects `160` bytes as an input that is interpreted as byte concatenation of encoding of G1 point (`128` bytes) and encoding of a scalar value (`32` bytes).
 	// > Output is an encoding of multiplication operation result - single G1 point (`128` bytes).
 	if len(input) != 160 {
@@ -1161,7 +1161,7 @@ func (c *bls12377G1Mul) Run(input []byte, caller common.Address, evm *EVM, gas u
 	return g.EncodePoint(r), gas, nil
 }
 
-// bls12377G1MultiExp implements EIP-2537 G1MultiExp precompile.
+// bls12377G1MultiExp implements EIP-2539 G1MultiExp precompile.
 type bls12377G1MultiExp struct{}
 
 // RequiredGas returns the gas required to execute the pre-compiled contract.
@@ -1184,7 +1184,7 @@ func (c *bls12377G1MultiExp) RequiredGas(input []byte) uint64 {
 }
 
 func (c *bls12377G1MultiExp) Run(input []byte, caller common.Address, evm *EVM, gas uint64) ([]byte, uint64, error) {
-	// Implements EIP-2537 G1MultiExp precompile.
+	// Implements EIP-2539 G1MultiExp precompile.
 	// G1 multiplication call expects `160*k` bytes as an input that is interpreted as byte concatenation of `k` slices each of them being a byte concatenation of encoding of G1 point (`128` bytes) and encoding of a scalar value (`32` bytes).
 	// Output is an encoding of multiexponentiation operation result - single G1 point (`128` bytes).
 	k := len(input) / 160
@@ -1218,7 +1218,7 @@ func (c *bls12377G1MultiExp) Run(input []byte, caller common.Address, evm *EVM, 
 	return g.EncodePoint(r), gas, nil
 }
 
-// bls12377G2Add implements EIP-2537 G2Add precompile.
+// bls12377G2Add implements EIP-2539 G2Add precompile.
 type bls12377G2Add struct{}
 
 // RequiredGas returns the gas required to execute the pre-compiled contract.
@@ -1227,7 +1227,7 @@ func (c *bls12377G2Add) RequiredGas(input []byte) uint64 {
 }
 
 func (c *bls12377G2Add) Run(input []byte, caller common.Address, evm *EVM, gas uint64) ([]byte, uint64, error) {
-	// Implements EIP-2537 G2Add precompile.
+	// Implements EIP-2539 G2Add precompile.
 	// > G2 addition call expects `512` bytes as an input that is interpreted as byte concatenation of two G2 points (`256` bytes each).
 	// > Output is an encoding of addition operation result - single G2 point (`256` bytes).
 	if len(input) != 512 {
@@ -1256,7 +1256,7 @@ func (c *bls12377G2Add) Run(input []byte, caller common.Address, evm *EVM, gas u
 	return g.EncodePoint(r), gas, nil
 }
 
-// bls12377G2Mul implements EIP-2537 G2Mul precompile.
+// bls12377G2Mul implements EIP-2539 G2Mul precompile.
 type bls12377G2Mul struct{}
 
 // RequiredGas returns the gas required to execute the pre-compiled contract.
@@ -1265,7 +1265,7 @@ func (c *bls12377G2Mul) RequiredGas(input []byte) uint64 {
 }
 
 func (c *bls12377G2Mul) Run(input []byte, caller common.Address, evm *EVM, gas uint64) ([]byte, uint64, error) {
-	// Implements EIP-2537 G2MUL precompile logic.
+	// Implements EIP-2539 G2MUL precompile logic.
 	// > G2 multiplication call expects `288` bytes as an input that is interpreted as byte concatenation of encoding of G2 point (`256` bytes) and encoding of a scalar value (`32` bytes).
 	// > Output is an encoding of multiplication operation result - single G2 point (`256` bytes).
 	if len(input) != 288 {
@@ -1292,7 +1292,7 @@ func (c *bls12377G2Mul) Run(input []byte, caller common.Address, evm *EVM, gas u
 	return g.EncodePoint(r), gas, nil
 }
 
-// bls12377G2MultiExp implements EIP-2537 G2MultiExp precompile.
+// bls12377G2MultiExp implements EIP-2539 G2MultiExp precompile.
 type bls12377G2MultiExp struct{}
 
 // RequiredGas returns the gas required to execute the pre-compiled contract.
@@ -1315,7 +1315,7 @@ func (c *bls12377G2MultiExp) RequiredGas(input []byte) uint64 {
 }
 
 func (c *bls12377G2MultiExp) Run(input []byte, caller common.Address, evm *EVM, gas uint64) ([]byte, uint64, error) {
-	// Implements EIP-2537 G2MultiExp precompile logic
+	// Implements EIP-2539 G2MultiExp precompile logic
 	// > G2 multiplication call expects `288*k` bytes as an input that is interpreted as byte concatenation of `k` slices each of them being a byte concatenation of encoding of G2 point (`256` bytes) and encoding of a scalar value (`32` bytes).
 	// > Output is an encoding of multiexponentiation operation result - single G2 point (`256` bytes).
 	k := len(input) / 288
@@ -1349,7 +1349,7 @@ func (c *bls12377G2MultiExp) Run(input []byte, caller common.Address, evm *EVM, 
 	return g.EncodePoint(r), gas, nil
 }
 
-// bls12377Pairing implements EIP-2537 Pairing precompile.
+// bls12377Pairing implements EIP-2539 Pairing precompile.
 type bls12377Pairing struct{}
 
 // RequiredGas returns the gas required to execute the pre-compiled contract.
@@ -1358,7 +1358,7 @@ func (c *bls12377Pairing) RequiredGas(input []byte) uint64 {
 }
 
 func (c *bls12377Pairing) Run(input []byte, caller common.Address, evm *EVM, gas uint64) ([]byte, uint64, error) {
-	// Implements EIP-2537 Pairing precompile logic.
+	// Implements EIP-2539 Pairing precompile logic.
 	// > Pairing call expects `384*k` bytes as an inputs that is interpreted as byte concatenation of `k` slices. Each slice has the following structure:
 	// > - `128` bytes of G1 point encoding
 	// > - `256` bytes of G2 point encoding
