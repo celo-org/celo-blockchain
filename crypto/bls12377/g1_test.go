f0d79c7ed7973b57c1d790b38a5436e499c25cc4
kilic <kiliconu@itu.edu.tr>
2020-10-19 13:48:21 +0300

    make mixed add private

--- crypto/bls12377/g1_test.go
+++ crypto/bls12377/g1_test.go
@@ -193,11 +193,11 @@ func TestG1MixedAdd(t *testing.T) {
 	t0, a := g.New(), g.rand()
 	zero := g.Zero()
 
-	g.AddMixed(t0, a, zero)
+	g.addMixed(t0, a, zero)
 	if !g.Equal(t0, a) {
 		t.Fatal("a + 0 == a")
 	}
-	g.AddMixed(a, t0, zero)
+	g.addMixed(a, t0, zero)
 	if !g.Equal(t0, a) {
 		t.Fatal("a + 0 == a")
 	}
@@ -215,13 +215,13 @@ func TestG1MixedAdd(t *testing.T) {
 		g.Affine(bAffine)
 		r0, r1 := g.New(), g.New()
 		g.Add(r0, a, b)
-		g.AddMixed(r1, a, bAffine)
+		g.addMixed(r1, a, bAffine)
 		if !g.Equal(r0, r1) {
 			t.Fatal("mixed addition failed")
 		}
 		aAffine := g.New().Set(a)
 		g.Affine(aAffine)
-		g.AddMixed(r0, a, aAffine)
+		g.addMixed(r0, a, aAffine)
 		g.Double(r1, a)
 		if !g.Equal(r0, r1) {
 			t.Fatal("mixed addition must double where points are equal")
@@ -324,7 +324,7 @@ func BenchmarkG1AddMixed(t *testing.B) {
 	g1.Affine(b)
 	t.ResetTimer()
 	for i := 0; i < t.N; i++ {
-		g1.AddMixed(&c, a, b)
+		g1.addMixed(&c, a, b)
 	}
 }
 
