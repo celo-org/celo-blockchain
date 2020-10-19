365446cc6bec70346d1b070c5c315f500b14d1b8
yoduyodu <wang7ong@gmail.com>
2020-10-19 22:41:35 +0800

    Fix var names

--- consensus/consensus.go
+++ consensus/consensus.go
@@ -144,14 +144,14 @@ type Handler interface {
 	SetP2PServer(P2PServer)
 
 	// RegisterPeer will notify the consensus engine that a new peer has been added
-	RegisterPeer(peer Peer, peerIsInternal bool) error
+	RegisterPeer(peer Peer, peerFromInternal bool) error
 
 	// UnregisterPeer will notify the consensus engine that a new peer has been removed
-	UnregisterPeer(peer Peer, peerIsInternal bool)
+	UnregisterPeer(peer Peer, peerFromInternal bool)
 
 	// Handshake will begin a handshake with a new peer. It returns if the peer
 	// has identified itself as a validator and should bypass any max peer checks.
-	Handshake(peer Peer, peerIsInternal bool) (bool, error)
+	Handshake(peer Peer, peerFromInternal bool) (bool, error)
 }
 
 // PoW is a consensus engine based on proof-of-work.
