6696bd15ef42fbe0043470a4156bae8bed9a62d8
Joshua Gutow <joshua@clabs.co>
2020-11-04 14:36:20 -0500

    Release celo-blockchain 1.2.0

--- params/version.go
+++ params/version.go
@@ -22,9 +22,9 @@ import (
 
 const (
 	VersionMajor = 1          // Major version component of the current release
-	VersionMinor = 0          // Minor version component of the current release
+	VersionMinor = 2          // Minor version component of the current release
 	VersionPatch = 0          // Patch version component of the current release
-	VersionMeta  = "unstable" // Version metadata to append to the version string
+	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
 type VersionInfo struct {
