d8ec1a9f56b5df7dc56f9a471d233e97f00157c2
James Prestwich <prestwich@clabs.co>
2021-01-29 11:28:13 -0800

    spec: resolve word length confusion by standardizing on EVM word

--- core/vm/contracts.go
+++ core/vm/contracts.go
@@ -1135,7 +1135,7 @@ func (c *getVerifiedSealBitmap) Run(input []byte, caller common.Address, evm *EV
 
 // cip20HashFunctions is a precompile to compute any of several
 // cryprographic hash functions
-type cip20HashFunctions struct{
+type cip20HashFunctions struct {
 	hashes map[uint8]Cip20Hash
 }
 
