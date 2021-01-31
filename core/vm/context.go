d1dcd641e686ed2521725889bd9a751c9bb69d16
Mariano Cortesi <mariano@clabs.co>
2021-01-30 21:55:22 -0300

    Refactor EVMContext to avoid depending from engine

--- core/vm/context.go
+++ core/vm/context.go
@@ -103,7 +103,8 @@ func NewEVMContext(msg Message, header *types.Header, chain ChainContext, txFeeR
 		BlockNumber:       new(big.Int).Set(header.Number),
 		Time:              new(big.Int).SetUint64(header.Time),
 		GasPrice:          new(big.Int).Set(msg.GasPrice()),
-		Engine:            engine,
+		EpochSize:         engine.EpochSize(),
+		GetValidators:     engine.GetValidators,
 	}
 }
 
