5f5ba2fa01596e764bb0fd63f23167f35507fdfb
Joshua Gutow <joshua@clabs.co>
2021-02-10 17:10:24 -0500

    Fix mycelo config tests

--- mycelo/config/config_test.go
+++ mycelo/config/config_test.go
@@ -6,8 +6,6 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/params"
-
 	. "github.com/onsi/gomega"
 )
 
@@ -15,19 +13,7 @@ func TestConfigMarhalling(t *testing.T) {
 	RegisterTestingT(t)
 
 	cfg := Config{
-		ChainID: big.NewInt(1500),
-		Istanbul: params.IstanbulConfig{
-			Epoch:          45555,
-			ProposerPolicy: 2,
-			LookbackWindow: 12,
-			BlockPeriod:    45,
-			RequestTimeout: 5000,
-		},
-		Hardforks: HardforkConfig{
-			ChurritoBlock: big.NewInt(1555),
-			DonutBlock:    big.NewInt(33333),
-		},
-		GenesisTimestamp:   9999,
+		ChainID:            big.NewInt(1500),
 		Mnemonic:           "aloha hawai",
 		InitialValidators:  6,
 		ValidatorsPerGroup: 2,
@@ -50,37 +36,13 @@ func TestConfigReadJson(t *testing.T) {
 
 	jsonStr := []byte(`{
 		"chainId": 1500,
-		"istanbul": {
-		 "epoch": 45555,
-		 "policy": 2,
-		 "lookbackwindow": 12,
-		 "blockperiod": 45,
-		 "requesttimeout": 5000
-		},
-		"hardforks": {
-		 "churritoBlock": 1555,
-		 "donutBlock": 33333
-		},
-		"genesisTimestamp": 9999,
 		"mnemonic": "aloha hawai",
 		"initialValidators": 6,
 		"validatorsPerGroup": 2
 	 }`)
 
 	expectedCfg := Config{
-		ChainID: big.NewInt(1500),
-		Istanbul: params.IstanbulConfig{
-			Epoch:          45555,
-			ProposerPolicy: 2,
-			LookbackWindow: 12,
-			BlockPeriod:    45,
-			RequestTimeout: 5000,
-		},
-		Hardforks: HardforkConfig{
-			ChurritoBlock: big.NewInt(1555),
-			DonutBlock:    big.NewInt(33333),
-		},
-		GenesisTimestamp:   9999,
+		ChainID:            big.NewInt(1500),
 		Mnemonic:           "aloha hawai",
 		InitialValidators:  6,
 		ValidatorsPerGroup: 2,
