10eb97ade7344421470a01d495dbf6cce3e9c39f
lucasege <lucas@celo.org>
2021-03-24 11:06:41 -0700

    Fix silly protocol bug because of bad code

--- eth/downloader/peer.go
+++ eth/downloader/peer.go
@@ -32,6 +32,7 @@ import (
 	"github.com/celo-org/celo-blockchain/common"
 	"github.com/celo-org/celo-blockchain/event"
 	"github.com/celo-org/celo-blockchain/log"
+	"github.com/celo-org/celo-blockchain/consensus/istanbul"
 )
 
 const (
@@ -478,7 +479,7 @@ func (ps *peerSet) HeaderIdlePeers() ([]*peerConnection, int) {
 		defer p.lock.RUnlock()
 		return p.headerThroughput
 	}
-	return ps.idlePeers(64, 66, idle, throughput)
+	return ps.idlePeers(istanbul.Celo64, istanbul.Celo67, idle, throughput)
 }
 
 // BodyIdlePeers retrieves a flat list of all the currently body-idle peers within
@@ -492,7 +493,7 @@ func (ps *peerSet) BodyIdlePeers() ([]*peerConnection, int) {
 		defer p.lock.RUnlock()
 		return p.blockThroughput
 	}
-	return ps.idlePeers(64, 66, idle, throughput)
+	return ps.idlePeers(istanbul.Celo64, istanbul.Celo67, idle, throughput)
 }
 
 // ReceiptIdlePeers retrieves a flat list of all the currently receipt-idle peers
@@ -506,7 +507,7 @@ func (ps *peerSet) ReceiptIdlePeers() ([]*peerConnection, int) {
 		defer p.lock.RUnlock()
 		return p.receiptThroughput
 	}
-	return ps.idlePeers(64, 66, idle, throughput)
+	return ps.idlePeers(istanbul.Celo64, istanbul.Celo67, idle, throughput)
 }
 
 // NodeDataIdlePeers retrieves a flat list of all the currently node-data-idle
@@ -520,7 +521,7 @@ func (ps *peerSet) NodeDataIdlePeers() ([]*peerConnection, int) {
 		defer p.lock.RUnlock()
 		return p.stateThroughput
 	}
-	return ps.idlePeers(64, 66, idle, throughput)
+	return ps.idlePeers(istanbul.Celo64, istanbul.Celo67, idle, throughput)
 }
 
 // idlePeers retrieves a flat list of all currently idle peers satisfying the
