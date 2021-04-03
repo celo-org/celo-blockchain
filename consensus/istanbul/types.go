45f1d16cc0cdf2e2bb729cfffdc4552aa2a9ad88
Asa Oines <a@celo.org>
2021-04-03 12:48:22 -0400

    Fix imports

--- consensus/istanbul/types.go
+++ consensus/istanbul/types.go
@@ -25,13 +25,13 @@ import (
 	"time"
 
 	"bitbucket.org/bertimus9/systemstat"
-	"github.com/celo-org/celo-blockchain/accounts"
-	"github.com/celo-org/celo-blockchain/common"
-	"github.com/celo-org/celo-blockchain/core/types"
-	"github.com/celo-org/celo-blockchain/crypto"
-	blscrypto "github.com/celo-org/celo-blockchain/crypto/bls"
-	"github.com/celo-org/celo-blockchain/p2p/enode"
-	"github.com/celo-org/celo-blockchain/rlp"
+	"github.com/ethereum/go-ethereum/accounts"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	blscrypto "github.com/ethereum/go-ethereum/crypto/bls"
+	"github.com/ethereum/go-ethereum/p2p/enode"
+	"github.com/ethereum/go-ethereum/rlp"
 )
 
 var Once sync.Once                                       // csv header printer
