7ccd823a4ad630b3c6806c647e961b8c56368c46
Piers Powlesland <pierspowlesland@gmail.com>
2021-04-22 12:36:07 +0100

    Add test package and first e2e test
    The test package makes it easy to test whole networks of nodes in a
    single go test process.

---
+++ e2e_test/e2e_test.go
@@ -0,0 +1,38 @@
+package e2e_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/celo-org/celo-blockchain/test"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	// This statement is commented out but left here since its very useful for
+	// debugging problems and its non trivial to construct.
+	//
+	//log.Root().SetHandler(log.LvlFilterHandler(log.LvlTrace, log.StreamHandler(os.Stdout, log.TerminalFormat(true))))
+}
+
+// This test starts a network submits a transaction and waits for the whole
+// network to process the transaction.
+func TestSendCelo(t *testing.T) {
+	accounts := test.Accounts(3)
+	gc := test.GenesisConfig(accounts)
+	network, err := test.NewNetwork(accounts, gc)
+	require.NoError(t, err)
+	defer network.Shutdown()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	// Send 1 celo from the dev account attached to node 0 to the dev account
+	// attached to node 1.
+	tx, err := network[0].SendCelo(ctx, network[1].DevAddress, 1)
+	require.NoError(t, err)
+
+	// Wait for the whole network to process the transaction.
+	err = network.AwaitTransactions(ctx, tx)
+	require.NoError(t, err)
+}
