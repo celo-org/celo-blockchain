a11798a3f69a927d0b288dd8e3b1fb12c0148757
Mariano Cortesi <mariano@clabs.co>
2021-06-03 17:27:22 -0300

    Run test in sequence due to data races

--- build/ci.go
+++ build/ci.go
@@ -350,7 +350,7 @@ func doTest(cmdline []string) {
 	// Test a single package at a time. CI builders are slow
 	// and some tests run into timeouts under load.
 	gotest := goTool("test", buildFlags(env)...)
-	// gotest.Args = append(gotest.Args, "-p", "1")
+	gotest.Args = append(gotest.Args, "-p", "1")
 	if *coverage {
 		gotest.Args = append(gotest.Args, "-covermode=atomic", "-cover", "-coverprofile=coverage.out")
 	}
