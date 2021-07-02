8d385e610ea077472766c4e402e7ab42763c3fc0
Or Neeman <or@clabs.co>
2021-07-02 08:38:13 -0600

    Remove 'Header' field from vm.Context.
    This was added in PR #317 but was never set in `vm.NewEVMContext()`.  As a result, it was always nil.  After the refactoring of contract calls to use the vm.EVMRunner types, we now don't use it outside of eth/api_tracer.go.  And the use there is now broken because the field is nil: when tracing a transaction or block the `vmRunner` created for the contract calls will use the header and state from the current chain head rather than from the specified block.  This fixes that by removing the field and updating the tracing code to not use it either.

--- eth/api.go
+++ eth/api.go
@@ -394,7 +394,7 @@ type storageEntry struct {
 
 // StorageRangeAt returns the storage at the given block height and transaction index.
 func (api *PrivateDebugAPI) StorageRangeAt(blockHash common.Hash, txIndex int, contractAddress common.Address, keyStart hexutil.Bytes, maxResult int) (StorageRangeResult, error) {
-	_, _, statedb, err := api.computeTxEnv(blockHash, txIndex, 0)
+	_, _, _, statedb, err := api.computeTxEnv(blockHash, txIndex, 0)
 	if err != nil {
 		return StorageRangeResult{}, err
 	}
