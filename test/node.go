f880dc86858ddef0e901157471d2fca116e6269f
Piers Powlesland <pierspowlesland@gmail.com>
2021-08-04 19:31:28 +0200

    Add some tactical sleeps to try to mitigate long connection process
    This seems to be working

--- test/node.go
+++ test/node.go
@@ -397,6 +397,7 @@ func NewNetwork(accounts *env.AccountsConfig, gc *genesis.Config) (Network, erro
 			network[j].Server().AddPeer(en, p2p.ValidatorPurpose)
 			network[j].Server().AddTrustedPeer(en, p2p.ValidatorPurpose)
 		}
+		time.Sleep(10 * time.Millisecond)
 	}
 
 	// Give nodes some time to connect. Also there is a race condition in
@@ -416,6 +417,7 @@ func NewNetwork(accounts *env.AccountsConfig, gc *genesis.Config) (Network, erro
 	for {
 		all2 := true
 		for _, n := range network {
+			n.Node.Server().PeerCount()
 			b := n.Eth.Engine().(*backend.Backend)
 			ps := len(b.GetPeers())
 			if ps > 2 {
@@ -428,6 +430,7 @@ func NewNetwork(accounts *env.AccountsConfig, gc *genesis.Config) (Network, erro
 		if all2 {
 			break
 		}
+		time.Sleep(time.Millisecond)
 	}
 	println("--------------------- peer finding took ", time.Since(start).String())
 
