e8b674392d7c60c4e13cfc3a95d9f293fec5db57
Or Neeman <or@clabs.co>
2021-08-18 08:43:27 -0600

    Fix calculation and implementation of dial ratio and the max dialed connections limit (#1670)
    * Fix split between dialed and incoming connections to take into account light clients are always incoming
    * Don't try to dial two peers for every one dialed peer we want to get

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -1202,6 +1202,9 @@ func SetP2PConfig(ctx *cli.Context, cfg *p2p.Config) {
 	if lightClient {
 		ethPeers = 0
 	}
+	if lightServer {
+		cfg.MaxLightClients = lightPeers
+	}
 	log.Info("Maximum peer count", "ETH", ethPeers, "LES", lightPeers, "total", cfg.MaxPeers)
 
 	if ctx.GlobalIsSet(MaxPendingPeersFlag.Name) {
