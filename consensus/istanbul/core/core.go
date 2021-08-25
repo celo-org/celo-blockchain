520932dc4aa076732f1201ebe59065b1b67ddd12
yoduyodu <wang7ong@gmail.com>
2021-08-25 17:14:10 -0500

    Again

--- consensus/istanbul/core/core.go
+++ consensus/istanbul/core/core.go
@@ -705,18 +705,18 @@ func (c *core) getRoundChangeTimeout() time.Duration {
 		Round n = baseTimeout + block time + 2^n * backoff factor
 
 		- Compare:
-		Round		before E     after E
-		0           8           8
-		1           5           10
-		2           7           12
-		3          11           16
-		4          19	        24
-		5          35	        40
-		6          67           72
+		Round     before E     after E
+		0         8            8
+		1         5            10
+		2         7            12
+		3         11           16
+		4         19	       24
+		5         35           40
+		6         67           72
 		7         131          136
 		8         259	       264
 		9         515	       520
-		10       1027	      1032
+		10        1027	       1032
 	*/
 	baseTimeout := time.Duration(c.config.RequestTimeout) * time.Millisecond
 	blockTime := time.Duration(c.config.BlockPeriod) * time.Second
