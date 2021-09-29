67e6a6aba3da82d3ee053fe651da07a144ea0fe3
Piers Powlesland <pierspowlesland@gmail.com>
2021-09-29 23:23:29 +0100

    Add start stop validators e2e test

--- e2e_test/e2e_test.go
+++ e2e_test/e2e_test.go
@@ -2,9 +2,11 @@ package e2e_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
+	"github.com/celo-org/celo-blockchain/core/types"
 	"github.com/celo-org/celo-blockchain/test"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -14,7 +16,7 @@ func init() {
 	// This statement is commented out but left here since its very useful for
 	// debugging problems and its non trivial to construct.
 	//
-	//log.Root().SetHandler(log.LvlFilterHandler(log.LvlTrace, log.StreamHandler(os.Stdout, log.TerminalFormat(true))))
+	// log.Root().SetHandler(log.LvlFilterHandler(log.LvlDebug, log.StreamHandler(os.Stdout, log.TerminalFormat(true))))
 }
 
 // This test starts a network submits a transaction and waits for the whole
@@ -65,4 +67,110 @@ func TestEpochBlockMarshaling(t *testing.T) {
 	// something there.
 	assert.True(t, len(b.EpochSnarkData().Signature) > 0)
 	assert.True(t, b.EpochSnarkData().Bitmap.Uint64() > 0)
+}
+
+// This test checks that a network can have validators shut down mid operation
+// and that it can continue to function, it also checks that if more than f
+// validators are shut down, when they restart the network is able to continue.
+func TestStartStopValidators(t *testing.T) {
+	accounts := test.Accounts(4)
+	gc, ec, err := test.BuildConfig(accounts)
+	require.NoError(t, err)
+	network, err := test.NewNetwork(accounts, gc, ec)
+	require.NoError(t, err)
+	defer network.Shutdown()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	var txs []*types.Transaction
+
+	// Send 1 celo from the dev account attached to node 0 to the dev account
+	// attached to node 1.
+	tx, err := network[0].SendCelo(ctx, network[1].DevAddress, 1)
+	require.NoError(t, err)
+	txs = append(txs, tx)
+
+	// Wait for the whole network to process the transaction.
+	err = network.AwaitTransactions(ctx, txs...)
+	require.NoError(t, err)
+
+	// Stop one node, the rest of the network should still be able to progress
+	err = network[3].Close()
+	require.NoError(t, err)
+
+	// Send 1 celo from the dev account attached to node 0 to the dev account
+	// attached to node 1.
+	tx, err = network[0].SendCelo(ctx, network[1].DevAddress, 1)
+	require.NoError(t, err)
+	txs = append(txs, tx)
+
+	// Check that the remaining network can still process this transction.
+	err = network[:3].AwaitTransactions(ctx, txs...)
+	require.NoError(t, err)
+
+	// Stop another node, the network should now be stuck
+	err = network[2].Close()
+	require.NoError(t, err)
+
+	// Now we will check that the network does not process transactions in this
+	// state, by waiting for a reasonable amount of time for it to process a
+	// transaction and assuming it is not processing transactions if we time out.
+	shortCtx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	// Send 1 celo from the dev account attached to node 0 to the dev account
+	// attached to node 1.
+	tx, err = network[0].SendCelo(shortCtx, network[1].DevAddress, 1)
+	require.NoError(t, err)
+	txs = append(txs, tx)
+
+	err = network[:2].AwaitTransactions(shortCtx, txs...)
+	// Expect DeadlineExceeded error
+	if !errors.Is(err, context.DeadlineExceeded) {
+		t.Fatalf("expecting %q, instead got: %v ", context.DeadlineExceeded.Error(), err)
+	}
+
+	// Start the last stopped node
+	err = network[2].Start()
+	require.NoError(t, err)
+
+	// Connect last stopped node to running nodes
+	network[2].AddPeers(network[:2]...)
+	time.Sleep(25 * time.Millisecond)
+	for _, n := range network[:3] {
+		err = n.GossipEnodeCertificatge()
+		require.NoError(t, err)
+	}
+
+	// Check that the  network now processes the previous transaction.
+	err = network[:3].AwaitTransactions(ctx, txs...)
+	require.NoError(t, err)
+
+	// Check that the network now quickly processes incoming transactions.
+	tx, err = network[0].SendCelo(ctx, network[1].DevAddress, 1)
+	require.NoError(t, err)
+	txs = append(txs, tx)
+
+	err = network[:3].AwaitTransactions(ctx, txs...)
+	require.NoError(t, err)
+
+	// Start the first stopped node
+	err = network[3].Start()
+	require.NoError(t, err)
+
+	// Connect final node to rest of network
+	network[3].AddPeers(network[:3]...)
+	time.Sleep(25 * time.Millisecond)
+	for _, n := range network {
+		err = n.GossipEnodeCertificatge()
+		require.NoError(t, err)
+	}
+
+	// Check that the network continues to quickly processes incoming transactions.
+	tx, err = network[0].SendCelo(ctx, network[1].DevAddress, 1)
+	require.NoError(t, err)
+	txs = append(txs, tx)
+
+	err = network.AwaitTransactions(ctx, txs...)
+	require.NoError(t, err)
+
 }
