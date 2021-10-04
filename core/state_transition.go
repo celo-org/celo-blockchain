d50bff1b20a17863e3b6a4d19d31fbeddb7d551e
yoduyodu <wang7ong@gmail.com>
2021-10-04 14:44:53 -0500

    Fix bug in precheck()

--- core/state_transition.go
+++ core/state_transition.go
@@ -430,8 +430,10 @@ func (st *StateTransition) preCheck() error {
 			}
 		}
 	} else { // Make sure this transaction's gas price is greater than baseFee (pre Espresso)
-		log.Debug("Tx gas price is less than minimum", "minimum", st.gasPriceMinimum, "price", st.gasPrice)
-		return ErrGasPriceDoesNotExceedMinimum
+		if st.gasPrice.Cmp(st.gasPriceMinimum) < 0 {
+			log.Debug("Tx gas price is less than minimum", "minimum", st.gasPriceMinimum, "price", st.gasPrice)
+			return ErrGasPriceDoesNotExceedMinimum
+		}
 	}
 
 	// // Make sure the sender is an EOA
