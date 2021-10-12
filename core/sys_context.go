98466cdd1ed8c86ae8f57a2a58aa99d2934db29a
yoduyodu <wang7ong@gmail.com>
2021-10-11 19:06:34 -0500

    Fix SCCC

--- core/sys_context.go
+++ core/sys_context.go
@@ -44,12 +44,14 @@ func (sc *SysContractCallCtx) IsWhitelisted(feeCurrency *common.Address) bool {
 		return true
 	}
 
-	whiteListedArr, err := currency.CurrencyWhitelist(sc.vmRunner)
-	if err != nil {
-		whiteListedArr = []common.Address{}
-	}
-	for _, feeCurrency := range whiteListedArr {
-		sc.whitelistedCurrencies[feeCurrency] = struct{}{}
+	if len(sc.whitelistedCurrencies) == 0 {
+		whiteListedArr, err := currency.CurrencyWhitelist(sc.vmRunner)
+		if err != nil {
+			whiteListedArr = []common.Address{}
+		}
+		for _, feeCurrency := range whiteListedArr {
+			sc.whitelistedCurrencies[feeCurrency] = struct{}{}
+		}
 	}
 
 	_, ok := sc.whitelistedCurrencies[*feeCurrency]
