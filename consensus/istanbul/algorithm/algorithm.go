1569f83d6f95b6d9c1026b9ef32ed1f45f5345f8
Piers Powlesland <pierspowlesland@gmail.com>
2021-12-13 13:58:46 +0000

    Update comments in ibft algorithm

--- consensus/istanbul/algorithm/algorithm.go
+++ consensus/istanbul/algorithm/algorithm.go
@@ -1,3 +1,4 @@
+// Package algorithm encapsulates logic pertaining the the celo ibft consensus algorithm.
 package algorithm
 
 import (
@@ -6,12 +7,16 @@ import (
 	"github.com/celo-org/celo-blockchain/common/hexutil"
 )
 
+// Value represents the value to be agreed by consensus. It's limited size
+// means that often the value here will be a hash or some similar reduction of
+// the real value to be agreed.
 type Value [32]byte
 
 func (v Value) String() string {
 	return hexutil.Encode(v[:])
 }
 
+// Type represents the different types of consensus messages.
 type Type uint8
 
 const (
@@ -21,6 +26,7 @@ const (
 	RoundChange
 )
 
+// In returns true if the method receiver is one of the given types.
 func (t Type) In(types ...Type) bool {
 	for _, x := range types {
 		if t == x {
@@ -45,12 +51,13 @@ func (t Type) String() string {
 	}
 }
 
+// State reprsents the discreet states that the algorithm can be in.
 type State uint8
 
-// We define these in a separate const block to the message types to maintain
-// the equivalence with the istanbul states. This is because the iota
-// identifier represents Successive untyped integer values within the scope of
-// a const block.
+// We define these states in a separate const block to the message types to
+// maintain the equivalence with the istanbul states (istanbul/core/types.go).
+// This is because the iota identifier represents Successive untyped integer
+// values within the scope of a const block.
 const (
 	AcceptRequest State = iota
 	Preprepared
@@ -59,6 +66,7 @@ const (
 	WaitingForNewRound
 )
 
+// Msg represents consensus messages.
 type Msg struct {
 	MsgType         Type
 	Height          uint64
@@ -71,6 +79,14 @@ func (m *Msg) String() string {
 	return fmt.Sprintf("T: %12v H: %12d R: %4d V: %v", m.MsgType, m.Height, m.Round, m.Val)
 }
 
+// Oracle provides access to external information. Allowing us to represent
+// concepts that are relevant to the consensus algorithm without needing to
+// have compile time dependencies on all the code required to manage an compute
+// those concepts. Currently it is managing more than it should be, since
+// certain logic that is really part of the consensus algorithm is abstracted
+// away by the Oracle, for example managing the round and state of the
+// algorithm and the logic for processing round change messages. In time we
+// should be able to move this logic here and slim down the oracle's interface.
 type Oracle interface {
 	CurrentState() State
 	QuorumCommit(height, round uint64, val Value) bool
@@ -81,10 +97,12 @@ type Oracle interface {
 	ValidRoundChangeCert(height, round uint64, val Value, rcc *Value) bool
 }
 
+// The algorithm struct, for now it only holds an oracle.
 type Algorithm struct {
 	O Oracle
 }
 
+// NewAlgorithm creates a new algorithm instance with the given oracle.
 func NewAlgorithm(o Oracle) *Algorithm {
 	return &Algorithm{
 		O: o,
@@ -92,10 +110,11 @@ func NewAlgorithm(o Oracle) *Algorithm {
 }
 
 // HandleMessage handles the given message and returns at most one of 3
-// results. A message indicates a state transition and that message needs to be
-// sent. A round indicates that the instance should move to that round if the
-// round is 0 it indicates that we should have committed. A desiredRound
-// indicates that the instance should wait for the desired round.
+// results. A message indicates a state transition has occurred and also that
+// the returned message needs to be broadcast. A round indicates that the
+// instance should move to that round, if the round is 0 it indicates that we
+// have committed. A desiredRound indicates that the instance should wait for
+// they desired round.
 func (a *Algorithm) HandleMessage(m *Msg) (msg *Msg, round, desiredRound *uint64) {
 	h := m.Height
 	r := m.Round
