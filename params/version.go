7950bf656f4b8b0725cdc6ea7fbf58c649180cf7
Gaston Ponti <ponti@clabs.co>
2022-01-12 14:29:18 -0300

    Release v1.5.0

--- params/version.go
+++ params/version.go
@@ -28,7 +28,7 @@ const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionMinor = 5        // Minor version component of the current release
 	VersionPatch = 0        // Patch version component of the current release
-	VersionMeta  = "beta.1" // Version metadata to append to the version string
+	VersionMeta  = "stable" // Version metadata to append to the version string
 )
 
 type VersionInfo struct {
