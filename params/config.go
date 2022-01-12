1b20e620b80a73305622519f6d7d002c76918413
Gaston Ponti <ponti@clabs.co>
2022-01-12 12:46:05 -0300

    Update activation block for Mainnet
    Time change from around 12:47 pm PDT to around 12:06 pm PDT

--- params/config.go
+++ params/config.go
@@ -64,7 +64,7 @@ var (
 		IstanbulBlock:       big.NewInt(0),
 		ChurritoBlock:       big.NewInt(6774000),
 		DonutBlock:          big.NewInt(6774000),
-		EspressoBlock:       big.NewInt(11355000),
+		EspressoBlock:       big.NewInt(11354500),
 		Istanbul: &IstanbulConfig{
 			Epoch:          17280,
 			ProposerPolicy: 2,
