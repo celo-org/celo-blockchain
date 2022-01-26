55f047d3243a376f7b616eec93e212a55118b716
Gaston Ponti <ponti@clabs.co>
2022-01-25 23:41:32 -0300

    Update Mainnet Espresso block activation

--- params/config.go
+++ params/config.go
@@ -64,7 +64,7 @@ var (
 		IstanbulBlock:       big.NewInt(0),
 		ChurritoBlock:       big.NewInt(6774000),
 		DonutBlock:          big.NewInt(6774000),
-		EspressoBlock:       big.NewInt(11354500),
+		EspressoBlock:       big.NewInt(11873000),
 		Istanbul: &IstanbulConfig{
 			Epoch:          17280,
 			ProposerPolicy: 2,
