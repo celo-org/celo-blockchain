4807c83833c8dee98318aec8a306465511880923
piersy <pierspowlesland@gmail.com>
2022-02-17 07:54:22 +0000

    Small adjustments to test (#1835)

--- e2e_test/e2e_test.go
+++ e2e_test/e2e_test.go
@@ -48,21 +48,21 @@ func TestSendCelo(t *testing.T) {
 // This test verifies correct behavior in a network of size one, in the case that
 // this fails we know that the problem does not lie with our network code.
 func TestSingleNodeNetworkManyTxs(t *testing.T) {
-	rounds := 100
-	txsPerRound := 5
+	iterations := 5
+	txsPerIteration := 5
 	ac := test.AccountConfig(1, 1)
 	gc, ec, err := test.BuildConfig(ac)
 	require.NoError(t, err)
-	gc.Istanbul.Epoch = uint64(rounds) * 50 // avoid the epoch for this test
+	gc.Istanbul.Epoch = uint64(iterations) * 50 // avoid the epoch for this test
 	network, shutdown, err := test.NewNetwork(ac, gc, ec)
 	require.NoError(t, err)
 	defer shutdown()
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
 	defer cancel()
 	accounts := test.Accounts(ac.DeveloperAccounts(), gc.ChainConfig())
-	for r := 0; r < rounds; r++ {
-		txs := make([]*types.Transaction, 0, txsPerRound)
-		for j := 0; j < txsPerRound; j++ {
+	for r := 0; r < iterations; r++ {
+		txs := make([]*types.Transaction, 0, txsPerIteration)
+		for j := 0; j < txsPerIteration; j++ {
 			tx, err := accounts[0].SendCelo(ctx, common.Address{}, 1, network[0])
 			require.NoError(t, err)
 			require.NotNil(t, tx)
