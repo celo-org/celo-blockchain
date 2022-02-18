c78a51f59dff06ff90355e22f0ae533f00cf7572
hbandura <hbandura@users.noreply.github.com>
2022-02-18 19:13:11 -0300

    Skipping flaky tests (#1848)
    This PR comments/skips tests, builds and benchmarks that are known to be flaky.
    
    Unit tests:
    - TestAnnounceGossipQueryMsg #1849
    - TestEthClient/TestCallContract #1850
    - TestGethClient/TestAccessList #1851
    
    Benchmarks:
    - BenchmarkNet1000Txs #1852
    
    Circleci builds:
    - end-to-end-cip35-eth-compatibility-test #1853
    
    
    While creating this PR, the istanbul-e2e-coverage failed once, then worked when it was re-run. I'm not skipping this one right now since I'd like to test it a bit more. Most likely it is because the parent commit has flaky tests, which are run for coverage report.
    
    codecov/project is failing because we are removing a few tests. It is expected.

--- e2e_test/e2e_test.go
+++ e2e_test/e2e_test.go
@@ -249,7 +249,7 @@ func TestRPCDynamicTxGasPriceWithState(t *testing.T) {
 	network, shutdown, err := test.NewNetwork(ac, gc, ec)
 	require.NoError(t, err)
 	defer shutdown()
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*90)
 	defer cancel()
 
 	accounts := test.Accounts(ac.DeveloperAccounts(), gc.ChainConfig())
@@ -306,7 +306,7 @@ func TestRPCDynamicTxGasPriceWithoutState(t *testing.T) {
 	network, shutdown, err := test.NewNetwork(ac, gc, ec)
 	require.NoError(t, err)
 	defer shutdown()
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*90)
 	defer cancel()
 
 	accounts := test.Accounts(ac.DeveloperAccounts(), gc.ChainConfig())
