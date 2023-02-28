a3d16d91e1c3278ec88bed1b49667edce9d48e09
Karl Bartel <karl.bartel@clabs.co>
2023-02-28 14:41:48 +0100

    Remove minimum client version test

--- contracts/blockchain_parameters/blockchain_parameters_test.go
+++ contracts/blockchain_parameters/blockchain_parameters_test.go
@@ -12,27 +12,6 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-func TestGetMinimumVersion(t *testing.T) {
-	testutil.TestFailOnFailingRunner(t, getMinimumVersion)
-	testutil.TestFailsWhenContractNotDeployed(t, contracts.ErrSmartContractNotDeployed, getMinimumVersion)
-
-	t.Run("should return minimum version", func(t *testing.T) {
-		g := NewGomegaWithT(t)
-
-		runner := testutil.NewSingleMethodRunner(
-			config.BlockchainParametersRegistryId,
-			"getMinimumClientVersion",
-			func() (*big.Int, *big.Int, *big.Int) {
-				return big.NewInt(5), big.NewInt(4), big.NewInt(3)
-			},
-		)
-
-		version, err := getMinimumVersion(runner)
-		g.Expect(err).ToNot(HaveOccurred())
-		g.Expect(version).To(Equal(&config.VersionInfo{Major: 5, Minor: 4, Patch: 3}))
-	})
-}
-
 func TestGetIntrinsicGasForAlternativeFeeCurrency(t *testing.T) {
 	testutil.TestFailOnFailingRunner(t, getIntrinsicGasForAlternativeFeeCurrency)
 	testutil.TestFailsWhenContractNotDeployed(t, contracts.ErrSmartContractNotDeployed, getIntrinsicGasForAlternativeFeeCurrency)
