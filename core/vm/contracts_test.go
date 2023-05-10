5ff0fbbbaf6d91ff9a4ea0b61cc007d5ddeb18a1
Paul Lange <paul.lange@clabs.co>
2023-05-10 15:58:53 +0200

    Remove bls12377 precompiles

--- core/vm/contracts_test.go
+++ core/vm/contracts_test.go
@@ -208,13 +208,6 @@ var allPrecompiles = map[common.Address]PrecompiledContract{
 	b12_381PairingAddress:    &bls12381Pairing{},
 	b12_381MapFpToG1Address:  &bls12381MapG1{},
 	b12_381MapFp2ToG2Address: &bls12381MapG2{},
-	b12_377G1AddAddress:      &bls12377G1Add{},
-	b12_377G1MulAddress:      &bls12377G1Mul{},
-	b12_377G1MultiExpAddress: &bls12377G1MultiExp{},
-	b12_377G2AddAddress:      &bls12377G2Add{},
-	b12_377G2MulAddress:      &bls12377G2Mul{},
-	b12_377G2MultiExpAddress: &bls12377G2MultiExp{},
-	b12_377PairingAddress:    &bls12377Pairing{},
 	cip26Address:             &getValidatorBLS{},
 }
 
@@ -533,69 +526,6 @@ func TestGetVerifiedSealBitmap(t *testing.T) {
 	for _, test := range getVerifiedSealBitmapTests {
 		testPrecompiled("f4", test, t)
 	}
-}
-
-func TestPrecompiledBLS12377G1Add(t *testing.T) {
-	testJSON("bls12377G1Add_matter", "e9", t)
-	testJSON("bls12377G1Add_zexe", "e9", t)
-}
-
-func TestPrecompiledBLS12377G1Mul(t *testing.T) {
-	testJSON("bls12377G1Mul_matter", "e8", t)
-	testJSON("bls12377G1Mul_zexe", "e8", t)
-}
-
-func TestPrecompiledBLS12377G1ZMultiExp(t *testing.T) {
-	testJSON("bls12377G1MultiExp_matter", "e7", t)
-	testJSON("bls12377G1MultiExp_zexe", "e7", t)
-}
-
-func TestPrecompiledBLS12377G2Add(t *testing.T) {
-	testJSON("bls12377G2Add_matter", "e6", t)
-	testJSON("bls12377G2Add_zexe", "e6", t)
-}
-
-func TestPrecompiledBLS12377G2Mul(t *testing.T) {
-	testJSON("bls12377G2Mul_matter", "e5", t)
-	testJSON("bls12377G2Mul_zexe", "e5", t)
-}
-
-func TestPrecompiledBLS12377G2MultiExp(t *testing.T) {
-	testJSON("bls12377G2MultiExp_matter", "e4", t)
-	testJSON("bls12377G2MultiExp_zexe", "e4", t)
-}
-
-func TestPrecompiledBLS12377Pairing(t *testing.T) {
-	testJSON("bls12377Pairing_matter", "e3", t)
-	testJSON("bls12377Pairing_zexe", "e3", t)
-}
-
-func TestPrecompiledBLS12377G1AddFail(t *testing.T) {
-	testJSONFail("fail-bls12377G1Add", "e9", t)
-}
-
-func TestPrecompiledBLS12377G1MulFail(t *testing.T) {
-	testJSONFail("fail-bls12377G1Mul", "e8", t)
-}
-
-func TestPrecompiledBLS12377G1MultiexpFail(t *testing.T) {
-	testJSONFail("fail-bls12377G1Multiexp", "e7", t)
-}
-
-func TestPrecompiledBLS12377G2AddFail(t *testing.T) {
-	testJSONFail("fail-bls12377G2Add", "e6", t)
-}
-
-func TestPrecompiledBLS12377G2MulFail(t *testing.T) {
-	testJSONFail("fail-bls12377G2Mul", "e5", t)
-}
-
-func TestPrecompiledBLS12377G2MultiexpFail(t *testing.T) {
-	testJSONFail("fail-bls12377G2Multiexp", "e4", t)
-}
-
-func TestPrecompiledBLS12377PairingFail(t *testing.T) {
-	testJSONFail("fail-bls12377Pairing", "e3", t)
 }
 
 func TestPrecompiledBLS12381G1Add(t *testing.T)      { testJSON("blsG1Add", "f2", t) }
