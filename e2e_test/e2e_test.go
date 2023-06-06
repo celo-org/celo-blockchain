f5b1801f009b11da00c7fdf91b44df70eb5727d4
Paul Lange <paul.lange@clabs.co>
2023-06-06 17:41:50 +0200

    Fix `TestTraceSendCeloViaGoldToken`

--- e2e_test/e2e_test.go
+++ e2e_test/e2e_test.go
@@ -89,8 +89,8 @@ func TestTraceSendCeloViaGoldToken(t *testing.T) {
 
 	require.NoError(t, err)
 	// Check top level gas values
-	require.Equal(t, result["gasUsed"], "0x3a1e")
-	require.Equal(t, result["gas"], "0x3a9b")
+	require.Equal(t, "0x45d6", result["gasUsed"])
+	require.Equal(t, "0x4653", result["gas"])
 }
 
 // This test verifies correct behavior in a network of size one, in the case that
