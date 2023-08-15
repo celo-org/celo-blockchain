3dcbc4fb1f8c85297eea66d9176d50b4d4f0e9c6
Gaston Ponti <ponti@clabs.co>
2023-08-15 00:08:13 -0300

    e2e tests using celo dynamic v1

--- test/node.go
+++ test/node.go
@@ -537,7 +537,7 @@ func ValueTransferTransactionWithDynamicFee(
 		return nil, fmt.Errorf("failed to estimate gas needed: %v", err)
 	}
 	// Create the transaction and sign it
-	rawTx := types.NewTx(&types.CeloDynamicFeeTxV2{
+	rawTx := types.NewTx(&types.CeloDynamicFeeTx{
 		Nonce:       nonce,
 		To:          &recipient,
 		Value:       value,
