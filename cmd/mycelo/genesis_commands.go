06290db54e1c9be30c6f2c56f8650719a6ab70f9
Paul Lange <palango@users.noreply.github.com>
2023-08-22 02:18:51 +0200

    "mycelo genesis" gingerbread activation fix (#2169)

--- cmd/mycelo/genesis_commands.go
+++ cmd/mycelo/genesis_commands.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path"
 
+	"github.com/celo-org/celo-blockchain/common"
 	"github.com/celo-org/celo-blockchain/internal/fileutils"
 	"github.com/celo-org/celo-blockchain/log"
 	"github.com/celo-org/celo-blockchain/mycelo/env"
@@ -44,23 +45,23 @@ var templateFlags = []cli.Flag{
 	},
 	cli.Int64Flag{
 		Name:  "forks.churrito",
-		Usage: "Optional flag to allow churrito fork overwritting (default: 0, disable: -1)",
+		Usage: "Optional flag to allow churrito fork overwriting (default: 0, disable: -1)",
 	},
 	cli.Int64Flag{
 		Name:  "forks.donut",
-		Usage: "Optional flag to allow donut fork overwritting (default: 0, disable: -1)",
+		Usage: "Optional flag to allow donut fork overwriting (default: 0, disable: -1)",
 	},
 	cli.Int64Flag{
 		Name:  "forks.espresso",
-		Usage: "Optional flag to allow espresso fork overwritting (default: 0, disable: -1)",
+		Usage: "Optional flag to allow espresso fork overwriting (default: 0, disable: -1)",
 	},
 	cli.Int64Flag{
 		Name:  "forks.gingerbread",
-		Usage: "Optional flag to allow gingerbread fork overwritting (default: 0, disable: -1)",
+		Usage: "Optional flag to allow gingerbread fork overwriting (default: 0, disable: -1)",
 	},
 	cli.Int64Flag{
 		Name:  "forks.gingerbreadp2",
-		Usage: "Optional flag to allow gingerbread p2 fork overwritting (default: 0, disable: -1)",
+		Usage: "Optional flag to allow gingerbread p2 fork overwriting (default: 0, disable: -1)",
 	},
 }
 
@@ -139,6 +140,8 @@ func envFromTemplate(ctx *cli.Context, workdir string) (*env.Environment, *genes
 		} else {
 			gingerbreadBlock = big.NewInt(gingerbreadBlockNumber)
 		}
+	} else {
+		gingerbreadBlock = common.Big1
 	}
 
 	// Genesis config
@@ -184,8 +187,6 @@ func envFromTemplate(ctx *cli.Context, workdir string) (*env.Environment, *genes
 			genesisConfig.Hardforks.EspressoBlock = big.NewInt(espressoBlockNumber)
 		}
 	}
-
-	genesisConfig.Hardforks.GingerbreadBlock = gingerbreadBlock
 
 	if ctx.IsSet("forks.gingerbreadp2") {
 		gingerbreadP2BlockNumber := ctx.Int64("forks.gingerbreadp2")
