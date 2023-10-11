cf340d98ffd46eeb96e836860ca409e044911df9
Javier Cortejoso <javier.cortejoso@gmail.com>
2023-10-11 09:47:25 +0200

    Update homedir test

--- cmd/utils/customflags_test.go
+++ cmd/utils/customflags_test.go
@@ -26,7 +26,7 @@ func TestPathExpansion(t *testing.T) {
 	user, _ := user.Current()
 	tests := map[string]string{
 		"/home/someuser/tmp": "/home/someuser/tmp",
-		"~/tmp":              user.HomeDir + "/tmp",
+		"~/tmp":              os.Getenv("HOME") + "/tmp",
 		"~thisOtherUser/b/":  "~thisOtherUser/b",
 		"$DDDXXX/a/b":        "/tmp/a/b",
 		"/a/b/":              "/a/b",
