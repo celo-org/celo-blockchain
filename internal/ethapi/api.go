9eeeb3d91a1b306ef0ae86ebd2a756d6f0bcdd49
Mohammed Sohail <sohailsameja@gmail.com>
2024-04-02 22:36:35 +0800

    reconcile: receipt changes from master 272982b (#2278)

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -2133,12 +2133,12 @@ func generateReceiptResponse(ctx context.Context, backend Backend, receipt *type
 			fields["effectiveGasPrice"] = hexutil.Uint64(tx.GasPrice().Uint64())
 		} else {
 			// var gasPrice *big.Int = new(big.Int)
-			if tx.Type() == types.DynamicFeeTxType || tx.Type() == types.CeloDynamicFeeTxType || tx.Type() == types.CeloDynamicFeeTxV2Type {
+			if tx.Type() == types.DynamicFeeTxType || tx.Type() == types.CeloDynamicFeeTxType || tx.Type() == types.CeloDynamicFeeTxV2Type || tx.Type() == types.CeloDenominatedTxType {
 				header, err := backend.HeaderByHash(ctx, blockHash)
 				if err != nil {
 					return nil, err
 				}
-				gasPriceMinimum, err := backend.GasPriceMinimumForHeader(ctx, tx.FeeCurrency(), header)
+				gasPriceMinimum, err := backend.GasPriceMinimumForHeader(ctx, tx.DenominatedFeeCurrency(), header)
 				if err == nil {
 					fields["effectiveGasPrice"] = hexutil.Uint64(new(big.Int).Add(gasPriceMinimum, tx.EffectiveGasTipValue(gasPriceMinimum)).Uint64())
 				}
